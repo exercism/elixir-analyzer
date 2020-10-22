ec930a603458077e0c84f31182c73837cd4addff
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-10-22 17:17:19 +0200

    Use quoted code instead of multiline strings

--- test/elixir_analyzer/exercise_test/two_fer_test.exs
+++ test/elixir_analyzer/exercise_test/two_fer_test.exs
@@ -9,36 +9,43 @@ defmodule ElixirAnalyzer.ExerciseTest.TwoFerTest do
       name: "missing moduledoc",
       status: :approve,
       comments: [Constants.solution_use_moduledoc()],
-      code: ~S"""
-        defmodule TwoFer do
-          @spec two_fer(String.t()) :: String.t()
-          def two_fer(name \\ "you") when is_binary(name) do
-            "One for #{name}, one for me"
+      code:
+        quote do
+          defmodule TwoFer do
+            @spec two_fer(String.t()) :: String.t()
+            def two_fer(name \\ "you") when is_binary(name) do
+              "One for #{name}, one for me"
+            end
           end
         end
-      """
     },
     %{
       name: "wrong spec",
       status: :refer,
-      comments: [Constants.solution_use_moduledoc(), Constants.two_fer_wrong_specification()],
+      comments: [Constants.two_fer_wrong_specification()],
       code: [
-        ~S"""
+        quote do
           defmodule TwoFer do
+            @moduledoc """
+            Two-fer or 2-fer is short for two for one. One for you and one for me.
+            """
             @spec two_fer(binary()) :: binary()
             def two_fer(name \\ "you") when is_binary(name) do
               "One for #{name}, one for me"
             end
           end
-        """,
-        ~S"""
+        end,
+        quote do
           defmodule TwoFer do
+            @moduledoc """
+            Two-fer or 2-fer is short for two for one. One for you and one for me.
+            """
             @spec two_fer(bitstring()) :: bitstring()
             def two_fer(name \\ "you") when is_binary(name) do
               "One for #{name}, one for me"
             end
           end
-        """
+        end
       ]
     }
   ]
@@ -49,7 +56,7 @@ defmodule ElixirAnalyzer.ExerciseTest.TwoFerTest do
     |> Enum.with_index()
     |> Enum.map(fn {code, index} ->
       test "#{test_case.name} - code sample #{index + 1}" do
-        result = TwoFer.analyze(empty_submission(), unquote(code))
+        result = TwoFer.analyze(empty_submission(), unquote(Macro.to_string(code)))
         assert result.status == unquote(test_case.status)
         assert result.comments == unquote(test_case.comments)
       end
