40f6c22d63bb985d3c6ee4d132f316d0681b26de
Tim Austin <tim@neenjaw.com>
2020-11-29 19:51:42 -0600

    simplify function signatures to global and local

--- lib/function_signature.ex
+++ lib/function_signature.ex
@@ -1,78 +1,53 @@
 defmodule FunctionSignature do
-  @enforce_keys [:module_path, :name, :arity]
-  defstruct [:module_path, :name, :arity]
+  @enforce_keys [:global, :name, :arity]
+  defstruct [:module_path, :global, :name, :arity]
 
   defmodule IncompleteFunctionSignatureError do
     defexception message: "function signature incomplete"
   end
 
   defimpl String.Chars do
-    def to_string(function_signature) do
+    def to_string(%{global: true} = signature) do
       module =
-        function_signature.module_path
+        signature.module_path
         |> Enum.join(".")
 
-      "#{module}.#{function_signature.name}/#{function_signature.arity}"
+      "&#{module}.#{signature.name}/#{signature.arity}"
+    end
+
+    def to_string(%{global: false} = signature) do
+      "&#{signature.name}/#{signature.arity}"
     end
   end
 
-  def parse(ast, opts \\ [local: false]) do
-    case do_parse(ast, opts) do
-      {nil, nil, nil} ->
+  def convert_to_local(%__MODULE__{} = signature) do
+    %{signature | global: false, module_path: nil}
+  end
+
+  def parse(ast) do
+    case do_parse(ast) do
+      {:global, module_path, name, arity} ->
+        %__MODULE__{global: true, module_path: module_path, name: name, arity: arity}
+
+      {:local, _, name, arity} ->
+        %__MODULE__{global: false, name: name, arity: arity}
+
+      _ ->
         raise IncompleteFunctionSignatureError
-
-      {_, nil, nil} ->
-        raise IncompleteFunctionSignatureError
-
-      {nil, _, nil} ->
-        raise IncompleteFunctionSignatureError
-
-      {nil, nil, _} ->
-        raise IncompleteFunctionSignatureError
-
-      {_, _, nil} ->
-        raise IncompleteFunctionSignatureError, "missing function arity from function signature"
-
-      {_, nil, _} ->
-        raise IncompleteFunctionSignatureError, "missing function name from function signature"
-
-      {nil, _, _} ->
-        raise IncompleteFunctionSignatureError, "missing function module from function signature"
-
-      {module_path, name, arity} ->
-        %__MODULE__{module_path: module_path, name: name, arity: arity}
     end
   end
 
-  defp do_parse(ast, opts) do
-    local = Keyword.get(opts, :local, false)
-
-    if local do
-      parse_local(ast, {nil, nil, nil})
-    else
-      parse_global(ast, {nil, nil, nil})
-    end
+  defp do_parse(
+         {:&, _, [{:/, _, [{{:., _, [{:__aliases__, _, module_path}, name]}, _, _}, arity]}]}
+       ) do
+    {:global, module_path, name, arity}
   end
 
-  @doc false
-  defp parse_local(_ast, parted) do
-    parted
+  defp do_parse({:&, _, [{:/, _, [{name, _, Elixir}, arity]}]}) do
+    {:local, nil, name, arity}
   end
 
-  @doc false
-  defp parse_global({:/, _, [signature_ast, arity]}, {module, function, _}) do
-    parse_global(signature_ast, {module, function, arity})
-  end
-
-  defp parse_global({{:., _, [{:__aliases__, _, module_path}, function]}, _, _}, {_, _, arity}) do
-    parse_global(nil, {module_path, function, arity})
-  end
-
-  defp parse_global({function, _, Elixir}, {module, _, arity}) do
-    {module, function, arity}
-  end
-
-  defp parse_global(_, signature) do
-    signature
+  defp do_parse(_) do
+    :error
   end
 end
