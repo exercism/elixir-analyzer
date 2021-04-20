8c02e1ef1079c1ea349e227c1ab7578a8041bcbc
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-04-20 21:14:06 +0200

    Add detecting aliases and imports

--- lib/test_suite/bird_count.ex
+++ lib/test_suite/bird_count.ex
@@ -12,10 +12,46 @@ defmodule ElixirAnalyzer.TestSuite.BirdCount do
     comment ElixirAnalyzer.Constants.bird_count_use_recursion()
   end
 
+  feature "does not alias or import Enum" do
+    find :none
+    type :essential
+    comment ElixirAnalyzer.Constants.bird_count_use_recursion()
+
+    form do
+      import Enum
+    end
+
+    form do
+      import Enum, _ignore
+    end
+
+    form do
+      alias Enum, as: _ignore
+    end
+  end
+
   assert_no_call "does not call any Stream functions" do
     type :essential
     called_fn module: Stream, name: :_
     comment ElixirAnalyzer.Constants.bird_count_use_recursion()
+  end
+
+  feature "does not alias or import Stream" do
+    find :none
+    type :essential
+    comment ElixirAnalyzer.Constants.bird_count_use_recursion()
+
+    form do
+      import Stream
+    end
+
+    form do
+      import Stream, _ignore
+    end
+
+    form do
+      alias Stream, as: _ignore
+    end
   end
 
   assert_no_call "does not call any List functions" do
@@ -24,7 +60,23 @@ defmodule ElixirAnalyzer.TestSuite.BirdCount do
     comment ElixirAnalyzer.Constants.bird_count_use_recursion()
   end
 
-  # TODO: also detect aliasing and importing those modules
+  feature "does not alias or import List" do
+    find :none
+    type :essential
+    comment ElixirAnalyzer.Constants.bird_count_use_recursion()
+
+    form do
+      import List
+    end
+
+    form do
+      import List, _ignore
+    end
+
+    form do
+      alias List, as: _ignore
+    end
+  end
 
   feature "doesn't use list comprehensions" do
     find :none
