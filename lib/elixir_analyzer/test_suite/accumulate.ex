c0c7e05a9ae10b550ed6b9f07615bf023f9e1515
Thiago Lima <thiagolima.unb@gmail.com>
2021-06-16 19:26:51 -0300

    Assert accumulate does not call Stream functions

--- lib/elixir_analyzer/test_suite/accumulate.ex
+++ lib/elixir_analyzer/test_suite/accumulate.ex
@@ -28,4 +28,28 @@ defmodule ElixirAnalyzer.TestSuite.Accumulate do
       alias Enum, as: _ignore
     end
   end
+
+  assert_no_call "does not call any Stream functions" do
+    type :essential
+    called_fn module: Stream, name: :_
+    comment ElixirAnalyzer.Constants.accumulate_use_recursion()
+  end
+
+  feature "does not alias or import Stream" do
+    find :none
+    type :essential
+    comment ElixirAnalyzer.Constants.accumulate_use_recursion()
+
+    form do
+      import Stream
+    end
+
+    form do
+      import Stream, _ignore
+    end
+
+    form do
+      alias Stream, as: _ignore
+    end
+  end
 end
