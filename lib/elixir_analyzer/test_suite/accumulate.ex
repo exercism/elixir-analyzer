3686205d50ec58fb61c29021f772d4ef2f3e103a
Thiago Lima <thiagolima.unb@gmail.com>
2021-06-16 19:20:59 -0300

    Assert accumulate does not call Enum functions

---
+++ lib/elixir_analyzer/test_suite/accumulate.ex
@@ -0,0 +1,31 @@
+defmodule ElixirAnalyzer.TestSuite.Accumulate do
+  @moduledoc """
+  This is an exercise analyzer extension module for the concept exercise Accumulate
+  """
+
+  use ElixirAnalyzer.ExerciseTest
+
+  assert_no_call "does not call any Enum functions" do
+    type :essential
+    called_fn module: Enum, name: :_
+    comment ElixirAnalyzer.Constants.accumulate_use_recursion()
+  end
+
+  feature "does not alias or import Enum" do
+    find :none
+    type :essential
+    comment ElixirAnalyzer.Constants.accumulate_use_recursion()
+
+    form do
+      import Enum
+    end
+
+    form do
+      import Enum, _ignore
+    end
+
+    form do
+      alias Enum, as: _ignore
+    end
+  end
+end
