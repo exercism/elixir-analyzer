c3fc46d0740de75f1bc265b61f252b89e62735ab
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-07-07 14:46:01 +0200

    Add analysis for `lasagna`

---
+++ lib/elixir_analyzer/test_suite/lasagna.ex
@@ -0,0 +1,21 @@
+defmodule ElixirAnalyzer.TestSuite.Lasagna do
+  @moduledoc """
+  This is an exercise analyzer extension module for the concept exercise Take-A-Number
+  """
+
+  use ElixirAnalyzer.ExerciseTest
+
+  assert_call "remaining minutes in oven are calculated based on expected minutes in oven" do
+    type :essential
+    calling_fn module: Lasagna, name: :remaining_minutes_in_oven
+    called_fn name: :expected_minutes_in_oven
+    comment ElixirAnalyzer.Constants.lasagna_function_reuse()
+  end
+
+  assert_call "total time in minutes is calculated based on preparation time in minutes" do
+    type :essential
+    calling_fn module: Lasagna, name: :total_time_in_minutes
+    called_fn name: :preparation_time_in_minutes
+    comment ElixirAnalyzer.Constants.lasagna_function_reuse()
+  end
+end
