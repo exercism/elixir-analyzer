1de05a7de7c96e3e8c40823dcdfd05899066441d
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-07-08 09:33:55 +0200

    Make comments actionable

--- lib/elixir_analyzer/test_suite/lasagna.ex
+++ lib/elixir_analyzer/test_suite/lasagna.ex
@@ -6,14 +6,14 @@ defmodule ElixirAnalyzer.TestSuite.Lasagna do
   use ElixirAnalyzer.ExerciseTest
 
   assert_call "remaining minutes in oven are calculated based on expected minutes in oven" do
-    type :essential
+    type :actionable
     calling_fn module: Lasagna, name: :remaining_minutes_in_oven
     called_fn name: :expected_minutes_in_oven
     comment ElixirAnalyzer.Constants.lasagna_function_reuse()
   end
 
   assert_call "total time in minutes is calculated based on preparation time in minutes" do
-    type :essential
+    type :actionable
     calling_fn module: Lasagna, name: :total_time_in_minutes
     called_fn name: :preparation_time_in_minutes
     comment ElixirAnalyzer.Constants.lasagna_function_reuse()
