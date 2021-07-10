6e9a93b98af3449b0e7fe8b4c9328019d9d4e9c8
Tim Austin <tim@neenjaw.com>
2021-07-09 20:00:05 -0600

    Update lib/elixir_analyzer/test_suite/default.ex

--- lib/elixir_analyzer/test_suite/default.ex
+++ lib/elixir_analyzer/test_suite/default.ex
@@ -3,7 +3,7 @@ defmodule ElixirAnalyzer.TestSuite.Default do
   This is the default exercise analyzer extension module.
 
   It will be run for any exercise submission that doesn't have its own extension module.
-  It's empty, which means it will only run the common checks.
+  It's intentionally empty, which means it will only run the common checks.
   """
 
   use ElixirAnalyzer.ExerciseTest
