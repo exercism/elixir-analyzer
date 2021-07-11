2a49529ff6a6ea11000b6b5fe82a5bbec201a66f
Francine Guimarães <francinesguimaraes@outlook.com>
2021-07-11 18:04:41 -0300

    Create run and traverse functions

--- lib/elixir_analyzer/exercise_test/common_checks/unless_with_else.ex
+++ lib/elixir_analyzer/exercise_test/common_checks/unless_with_else.ex
@@ -4,4 +4,64 @@ defmodule ElixirAnalyzer.ExerciseTest.CommonChecks.UnlessWithElse do
 
   Common check to be run on every single solution.
   """
+  alias ElixirAnalyzer.Constants
+  alias ElixirAnalyzer.Comment
+
+  @def_ops [:def, :defp, :defmacro, :defmacrop, :defguard, :defguardp]
+
+  @spec run(Macro.t()) :: [{:pass | :fail | :skip, %Comment{}}]
+  def run(ast) do
+    {_, issues} = Macro.prewalk(ast, [], &traverse/2)
+
+    # correct_implementation = if_else_implementation()
+    # wrong_implementation = issues
+
+    # if else_block? do
+    # [
+    #   {:fail,
+    #    %Comment{
+    #      type: :actionable,
+    #      comment: Constants.solution_unless_with_else(),
+    #      params: %{
+    #        expected: correct_implementation,
+    #        actual: wrong_implementation
+    #      }
+    #    }}
+    # ]
+    # end
+  end
+
+  defp traverse({:unless, meta, args} = ast, issues) do
+    if else_block?(args) do
+      a = {ast, issues}
+      IO.inspect(a, label: "a")
+    else
+      nil
+    end
+  end
+
+  defp else_block?(args) do
+    case else_block_for(args) do
+      {:ok, else_block} -> else_block
+      nil -> nil
+    end
+  end
+
+  defp else_block_for(args) when is_list(args) do
+    Enum.find_value(args, &find_keyword(&1, :else))
+  end
+
+  defp if_else_implementation({:unless, meta, args} = ast) do
+    [_, [do: do_block, else: else_block] = block] = args
+  end
+
+  defp find_keyword(list, keyword) when is_list(list) do
+    if Keyword.has_key?(list, keyword) do
+      {:ok, list[keyword]}
+    else
+      nil
+    end
+  end
+
+  defp find_keyword(_, _), do: nil
 end
