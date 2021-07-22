c37bf5d0a03d1052131e5e367cffc677beb4eb30
Jérémie Gillet <jie.gillet@gmail.com>
2021-07-22 22:17:57 +0900

    Add tests for importing available functions

--- test/elixir_analyzer/exercise_test/assert_call/module_tracking_test.exs
+++ test/elixir_analyzer/exercise_test/assert_call/module_tracking_test.exs
@@ -3,7 +3,7 @@ defmodule ElixirAnalyzer.ExerciseTest.AssertCall.ModuleTrackingTest do
     exercise_test_module: ElixirAnalyzer.Support.AnalyzerVerification.AssertCall.ModuleTracking
 
   test_exercise_analysis "Calling Elixir.Mix.Utils.read_path/1",
-    comments: [
+    comments_include: [
       "found a call to Elixir.Mix.Utils.read_path/1"
     ] do
     [
@@ -38,13 +38,6 @@ defmodule ElixirAnalyzer.ExerciseTest.AssertCall.ModuleTrackingTest do
 
         def function() do
           {:ok, file} = Utils.read_path("mix.exs")
-        end
-      end,
-      defmodule AssertCallVerification do
-        import Elixir
-
-        def function() do
-          {:ok, file} = Mix.Utils.read_path("mix.exs")
         end
       end,
       defmodule AssertCallVerification do
@@ -144,25 +137,9 @@ defmodule ElixirAnalyzer.ExerciseTest.AssertCall.ModuleTrackingTest do
         def function() do
           {:ok, file} = Elixir.Mix.Utils.read_path("mix.exs")
         end
-      end
-    ]
-  end
-
-  # In general, we have no way of differentiating macros and functions
-  test_exercise_analysis "Theimport options only: :macros and only: :functions are ignored",
-    comments: [
-      "found a call to Elixir.Mix.Utils.read_path/1"
-    ] do
-    [
-      defmodule AssertCallVerification do
-        import Elixir.Mix.Utils, only: :functions
-
-        def function() do
-          {:ok, file} = read_path("mix.exs")
-        end
       end,
       defmodule AssertCallVerification do
-        import Elixir.Mix.Utils, only: :macros
+        import Elixir.Mix.Utils, only: :functions
 
         def function() do
           {:ok, file} = read_path("mix.exs")
@@ -172,12 +149,19 @@ defmodule ElixirAnalyzer.ExerciseTest.AssertCall.ModuleTrackingTest do
   end
 
   test_exercise_analysis "Not calling Elixir.Mix.Utils.read_path/1",
-    comments: [
+    comments_include: [
       "didn't find any call to Elixir.Mix.Utils.read_path/1"
     ] do
     [
       defmodule AssertCallVerification do
         def function() do
+        end
+      end,
+      defmodule AssertCallVerification do
+        import Elixir.Mix.Utils, only: :macros
+
+        def function() do
+          {:ok, file} = read_path("mix.exs")
         end
       end,
       defmodule AssertCallVerification do
@@ -203,8 +187,6 @@ defmodule ElixirAnalyzer.ExerciseTest.AssertCall.ModuleTrackingTest do
         end
       end,
       defmodule AssertCallVerification do
-        import Elixir
-
         def function() do
           {:ok, file} = Utils.read_path("mix.exs")
         end
@@ -218,6 +200,13 @@ defmodule ElixirAnalyzer.ExerciseTest.AssertCall.ModuleTrackingTest do
       end,
       defmodule AssertCallVerification do
         import Elixir.Mix.Utils, only: [read_path: 0, read_path: 2, read_path: 3]
+
+        def function() do
+          {:ok, file} = read_path("mix.exs")
+        end
+      end,
+      defmodule AssertCallVerification do
+        import Elixir.Mix.Utils, except: [read_path: 1]
 
         def function() do
           {:ok, file} = read_path("mix.exs")
@@ -292,6 +281,54 @@ defmodule ElixirAnalyzer.ExerciseTest.AssertCall.ModuleTrackingTest do
           {:ok, file} = E.Mix.Utils.read_path("mix.exs")
           alias Elixir, as: E
         end
+      end
+    ]
+  end
+
+  test_exercise_analysis "Calling non standard library function using import",
+    comments_include: [
+      "found a call to MyModule.Custom.my_function/0"
+    ] do
+    [
+      defmodule AssertCallVerification do
+        import MyModule
+        Custom.my_function()
+      end,
+      defmodule AssertCallVerification do
+        import MyModule.Custom, only: [my_function: 0]
+        my_function()
+      end
+    ]
+  end
+
+  test_exercise_analysis "Failing to call non standard library function using import",
+    comments_include: [
+      "didn't find any call to MyModule.Custom.my_function/0"
+    ] do
+    [
+      defmodule AssertCallVerification do
+        import MyModule.Custom
+        my_function()
+      end,
+      defmodule AssertCallVerification do
+        import MyModule.Custom, only: :functions
+        my_function()
+      end,
+      defmodule AssertCallVerification do
+        import MyModule.Custom, only: :macros
+        my_function()
+      end,
+      defmodule AssertCallVerification do
+        import MyModule.Custom, except: [my_function: 0]
+        my_function()
+      end,
+      defmodule AssertCallVerification do
+        import MyModule.Custom, except: [her_function: 0]
+        my_function()
+      end,
+      defmodule AssertCallVerification do
+        import MyModule.Custom, only: [her_function: 0]
+        my_function()
       end
     ]
   end
--- test/support/analyzer_verification/assert_call/module_tracking.ex
+++ test/support/analyzer_verification/assert_call/module_tracking.ex
@@ -12,23 +12,21 @@ defmodule ElixirAnalyzer.Support.AnalyzerVerification.AssertCall.ModuleTracking 
     comment "didn't find any call to Elixir.Mix.Utils.read_path/1"
   end
 
-  #  assert_call "find a call to Elixir.Mix.Utils.read_path/1 in function/0" do
-  #    type :informational
-  #    called_fn module: Elixir.Mix.Utils, name: :read_path
-  #    calling_fn module: AssertCallVerification, name: :function
-  #    comment "didn't find a call to Elixir.Mix.Utils.read_path/1 in function/0"
-  #  end
-
   assert_no_call "didn't call to Elixir.Mix.Utils.read_path/1 from anywhere" do
     type :informational
     called_fn module: Elixir.Mix.Utils, name: :read_path
     comment "found a call to Elixir.Mix.Utils.read_path/1"
   end
 
-  #  assert_no_call "didn't call to Elixir.Mix.Utils.read_path/1 in function/0" do
-  #    type :informational
-  #    called_fn module: Elixir.Mix.Utils, name: :read_path
-  #    calling_fn module: AssertCallVerification, name: :function
-  #    comment "found a call to Elixir.Mix.Utils.read_path/1 in function/0"
-  #  end
+  assert_call "find a call to a custom MyModule.Custom.my_function/0 from anywhere" do
+    type :informational
+    called_fn module: MyModule.Custom, name: :my_function
+    comment "didn't find any call to MyModule.Custom.my_function/0"
+  end
+
+  assert_no_call "didn't call custom MyModule.Custom.my_function/0 from anywhere" do
+    type :informational
+    called_fn module: MyModule.Custom, name: :my_function
+    comment "found a call to MyModule.Custom.my_function/0"
+  end
 end
