0460a808f95e5dfcfb8edf2e55dcc2d234a244d9
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-08-04 19:21:46 +0200

    Fix variable name check not reporting when using guards

--- test/elixir_analyzer/exercise_test/common_checks/function_names_test.exs
+++ test/elixir_analyzer/exercise_test/common_checks/function_names_test.exs
@@ -40,8 +40,8 @@ defmodule ElixirAnalyzer.ExerciseTest.CommonChecks.FunctionNamesTest do
           defp registered_in_last_quarter?(user), do: true
           defmacro validate_user!, do: true
           defmacrop do_validate_user, do: true
-          defguard(is_user, do: true)
-          defguardp(is_registered_user, do: true)
+          defguard is_user when true
+          defguardp is_registered_user when true
         end
       end
 
@@ -136,7 +136,7 @@ defmodule ElixirAnalyzer.ExerciseTest.CommonChecks.FunctionNamesTest do
       quote do
         defmodule User do
           def first_name(user), do: getFirstName(user)
-          defguard(isRegisteredUser(user), do: true)
+          defguard isRegisteredUser(user) when true
           defp getFirstName(user), do: user.first_name
         end
       end
