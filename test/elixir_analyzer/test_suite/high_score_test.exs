0a052f54cbc67f2ae7b53e7901da3fc52d9e6251
Jie <jie.gillet@gmail.com>
2021-09-04 14:50:17 +0900

    [#134] `high-score` analysis should allow reset_score with remove + add player (#138)

--- test/elixir_analyzer/test_suite/high_score_test.exs
+++ test/elixir_analyzer/test_suite/high_score_test.exs
@@ -4,31 +4,58 @@ defmodule ElixirAnalyzer.ExerciseTest.HighScoreTest do
 
   test_exercise_analysis "example solution",
     comments: [] do
-    defmodule HighScore do
-      @initial_score 0
+    [
+      defmodule HighScore do
+        @initial_score 0
 
-      def new(), do: %{}
+        def new(), do: %{}
 
-      def add_player(scores, name, score \\ @initial_score) do
-        Map.put(scores, name, score)
+        def add_player(scores, name, score \\ @initial_score) do
+          Map.put(scores, name, score)
+        end
+
+        def remove_player(scores, name) do
+          Map.delete(scores, name)
+        end
+
+        def reset_score(scores, name) do
+          Map.put(scores, name, @initial_score)
+        end
+
+        def update_score(scores, name, score) do
+          Map.update(scores, name, score, &(&1 + score))
+        end
+
+        def get_players(scores) do
+          Map.keys(scores)
+        end
+      end,
+      defmodule HighScore do
+        @initial_score 0
+
+        def new(), do: %{}
+
+        def add_player(scores, name, score \\ @initial_score) do
+          Map.put(scores, name, score)
+        end
+
+        def remove_player(scores, name) do
+          Map.delete(scores, name)
+        end
+
+        def reset_score(scores, name) do
+          scores |> remove_player(name) |> add_player(name)
+        end
+
+        def update_score(scores, name, score) do
+          Map.update(scores, name, score, &(&1 + score))
+        end
+
+        def get_players(scores) do
+          Map.keys(scores)
+        end
       end
-
-      def remove_player(scores, name) do
-        Map.delete(scores, name)
-      end
-
-      def reset_score(scores, name) do
-        Map.put(scores, name, @initial_score)
-      end
-
-      def update_score(scores, name, score) do
-        Map.update(scores, name, score, &(&1 + score))
-      end
-
-      def get_players(scores) do
-        Map.keys(scores)
-      end
-    end
+    ]
   end
 
   test_exercise_analysis "requires add_player to have a default argument that's a module attribute",
