f1ef096653cf3146c87c48d9ac11b8dcbe177516
Maurizio Pasquinelli <pasquinellimaurizio@gmail.com>
2021-09-06 10:09:21 +0200

    Add more dots two_fer

--- test/support/exercise_test_case.ex
+++ test/support/exercise_test_case.ex
@@ -34,7 +34,7 @@ defmodule ElixirAnalyzer.ExerciseTestCase do
       defmodule TwoFer do
         @spec two_fer(String.t()) :: String.t()
         def two_fer(name \\ "you") when is_binary(name) do
-          "One for #{name}, one for me"
+          "One for #{name}, one for me."
         end
       end
     end
