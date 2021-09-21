899fa938f615ad2c6284e1d5c9e38d1c93fba8ba
Jie <jie.gillet@gmail.com>
2021-09-21 22:40:33 +0900

    Detect assignment on last line of function/macro definition (#153)

--- lib/elixir_analyzer/constants.ex
+++ lib/elixir_analyzer/constants.ex
@@ -29,6 +29,7 @@ defmodule ElixirAnalyzer.Constants do
     solution_last_line_assignment: "elixir.solution.last_line_assignment",
     solution_compiler_warnings: "elixir.solution.compiler_warnings",
     solution_same_as_exemplar: "elixir.solution.same_as_exemplar",
+    solution_last_line_assignment: "elixir.solution.last_line_assignment",
 
     # Concept exercises
 
