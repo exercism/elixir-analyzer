29aa034e4b8068a9baa6458e9185f1f4ca8ec807
Oleksii Filonenko <brightone@protonmail.com>
2021-09-22 00:27:02 +0300

    Make both log-level asserts essential

--- lib/elixir_analyzer/test_suite/log_level.ex
+++ lib/elixir_analyzer/test_suite/log_level.ex
@@ -6,14 +6,14 @@ defmodule ElixirAnalyzer.TestSuite.LogLevel do
   use ElixirAnalyzer.ExerciseTest
 
   assert_call "to_label uses cond/1" do
-    type :actionable
+    type :essential
     comment ElixirAnalyzer.Constants.log_level_use_cond()
     calling_fn module: LogLevel, name: :to_label
     called_fn name: :cond
   end
 
   assert_call "alert_recipient uses cond/1" do
-    type :actionable
+    type :essential
     comment ElixirAnalyzer.Constants.log_level_use_cond()
     calling_fn module: LogLevel, name: :alert_recipient
     called_fn name: :cond
