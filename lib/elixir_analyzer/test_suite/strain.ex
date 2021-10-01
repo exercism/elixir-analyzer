79318c51725e008b0b782edea73792df50b14de6
Oleksii Filonenko <brightone@protonmail.com>
2021-10-01 18:00:26 +0300

    Alias ElixirAnalyzer.Constants

--- lib/elixir_analyzer/test_suite/strain.ex
+++ lib/elixir_analyzer/test_suite/strain.ex
@@ -4,34 +4,35 @@ defmodule ElixirAnalyzer.TestSuite.Strain do
   """
 
   use ElixirAnalyzer.ExerciseTest
+  alias ElixirAnalyzer.Constants
 
   assert_no_call "does not call Enum.filter/2" do
     type :essential
     called_fn module: Enum, name: :filter
-    comment ElixirAnalyzer.Constants.strain_use_recursion()
+    comment Constants.strain_use_recursion()
   end
 
   assert_no_call "does not call Enum.reject/2" do
     type :essential
     called_fn module: Enum, name: :reject
-    comment ElixirAnalyzer.Constants.strain_use_recursion()
+    comment Constants.strain_use_recursion()
   end
 
   assert_no_call "does not call Stream.filter/2" do
     type :essential
     called_fn module: Stream, name: :filter
-    comment ElixirAnalyzer.Constants.strain_use_recursion()
+    comment Constants.strain_use_recursion()
   end
 
   assert_no_call "does not call Stream.reject/2" do
     type :essential
     called_fn module: Stream, name: :reject
-    comment ElixirAnalyzer.Constants.strain_use_recursion()
+    comment Constants.strain_use_recursion()
   end
 
   assert_no_call "doesn't use list comprehensions" do
     type :essential
     called_fn name: :for
-    comment ElixirAnalyzer.Constants.strain_use_recursion()
+    comment Constants.strain_use_recursion()
   end
 end
