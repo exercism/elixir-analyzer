073b0276f24af3561b3b3842a5ee1e3a561fc8d3
José Eduardo <51955049+joseemds@users.noreply.github.com>
2021-10-04 13:15:45 -0300

    Add captains log analyzer (#184)
    Co-authored-by: Angelika Tyborska <angelikatyborska@fastmail.com>

---
+++ lib/elixir_analyzer/test_suite/captains_log.ex
@@ -0,0 +1,42 @@
+defmodule ElixirAnalyzer.TestSuite.CaptainsLog do
+  @moduledoc """
+  This is an exercise analyzer extension module for the concept exercise Captains Log
+  """
+  use ElixirAnalyzer.ExerciseTest
+  alias ElixirAnalyzer.Constants
+
+  assert_call "random_planet_class uses Enum.random" do
+    type :essential
+    calling_fn module: CaptainsLog, name: :random_planet_class
+    called_fn module: Enum, name: :random
+    comment Constants.captains_log_use_enum_random()
+  end
+
+  assert_call "random_ship_registry_number uses Enum.random" do
+    type :essential
+    calling_fn module: CaptainsLog, name: :random_ship_registry_number
+    called_fn module: Enum, name: :random
+    comment Constants.captains_log_use_enum_random()
+  end
+
+  assert_no_call "random_stardate does not use Enum.random" do
+    type :essential
+    calling_fn module: CaptainsLog, name: :random_stardate
+    called_fn module: Enum, name: :random
+    comment Constants.captains_log_use_rand_uniform()
+  end
+
+  assert_call "random_stardate uses :rand.uniform" do
+    type :essential
+    calling_fn module: CaptainsLog, name: :random_stardate
+    called_fn module: :rand, name: :uniform
+    comment Constants.captains_log_use_rand_uniform()
+  end
+
+  assert_call "format_stardate uses :io_lib" do
+    type :essential
+    calling_fn module: CaptainsLog, name: :format_stardate
+    called_fn module: :io_lib, name: :_
+    comment Constants.captains_log_use_io_lib()
+  end
+end
---
+++ test/elixir_analyzer/test_suite/captains_log_test.exs
@@ -0,0 +1,81 @@
+defmodule ElixirAnalyzer.ExerciseTest.CaptainsLogTest do
+  use ElixirAnalyzer.ExerciseTestCase,
+    exercise_test_module: ElixirAnalyzer.TestSuite.CaptainsLog
+
+  alias ElixirAnalyzer.Constants
+
+  test_exercise_analysis "example solution",
+    comments: [Constants.solution_same_as_exemplar()] do
+    defmodule CaptainsLog do
+      @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
+
+      def random_planet_class() do
+        Enum.random(@planetary_classes)
+      end
+
+      def random_ship_registry_number() do
+        number = Enum.random(1000..9999)
+        "NCC-#{number}"
+      end
+
+      def random_stardate() do
+        :rand.uniform() * 1000 + 41_000
+      end
+
+      def format_stardate(stardate) do
+        to_string(:io_lib.format("~.1f", [stardate]))
+      end
+    end
+  end
+
+  describe "Expects random_planet_class and random_ship_registry_number to use Enum.random" do
+    test_exercise_analysis "random_planet_class does not use Enum.random",
+      comments_include: [Constants.captains_log_use_enum_random()] do
+      defmodule CaptainsLog do
+        @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
+
+        def random_planet_class() do
+          @planetary_classes
+          |> Enum.shuffle()
+          |> hd
+        end
+      end
+    end
+
+    test_exercise_analysis "random_ship_registry_number does not use Enum.random",
+      comments_include: [Constants.captains_log_use_enum_random()] do
+      defmodule CaptainsLog do
+        def random_ship_registry_number() do
+          number =
+            1000..9999
+            |> Enum.shuffle()
+            |> hd
+
+          "NCC-#{number}"
+        end
+      end
+    end
+  end
+
+  test_exercise_analysis "random_stardate uses :rand.uniform",
+    comments_include: [Constants.captains_log_use_rand_uniform()] do
+    defmodule CaptainsLog do
+      def random_stardate do
+        Enum.random(4_100_000..4_200_000) |> Kernel./(100)
+      end
+    end
+  end
+
+  test_exercise_analysis "format_stardate uses Float.round",
+    comments_include: [Constants.captains_log_use_io_lib()] do
+    defmodule CaptainsLog do
+      def format_stardate(stardate) do
+        if is_float(stardate) do
+          Float.round(stardate, 1) |> to_string()
+        else
+          raise ArgumentError
+        end
+      end
+    end
+  end
+end
