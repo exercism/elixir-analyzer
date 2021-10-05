8d49149793aafc966041d55c705d6821d5d3b435
José Eduardo <josee.edumont@gmail.com>
2021-10-04 21:17:02 -0300

    Add more test to `DocSpecOrder` Common Check

--- test/elixir_analyzer/exercise_test/common_checks/doc_spec_order_test.exs
+++ test/elixir_analyzer/exercise_test/common_checks/doc_spec_order_test.exs
@@ -4,7 +4,20 @@ defmodule ElixirAnalyzer.ExerciseTest.CommonChecks.DocSpecOrderTest do
   alias ElixirAnalyzer.Constants
   alias ElixirAnalyzer.ExerciseTest.CommonChecks.DocSpecOrder
 
-  test "wrong order" do
+  setup_all do
+    error = [
+      {:fail,
+       %Comment{
+         type: :informative,
+         name: Constants.solution_doc_spec_order(),
+         comment: Constants.solution_doc_spec_order()
+       }}
+    ]
+
+    [error: error]
+  end
+
+  test "wrong order crashes", %{error: error} do
     ast =
       quote do
         defmodule Test do
@@ -14,13 +27,138 @@ defmodule ElixirAnalyzer.ExerciseTest.CommonChecks.DocSpecOrderTest do
         end
       end
 
-    assert DocSpecOrder.run(ast) == [
-             {:fail,
-              %Comment{
-                type: :informative,
-                name: Constants.solution_doc_spec_order(),
-                comment: Constants.solution_doc_spec_order()
-              }}
-           ]
+    assert DocSpecOrder.run(ast) == error
+  end
+
+  test "works for def, defp, defmacro, defmacrop, defguard, and defguardp", %{error: error} do
+    ast =
+      quote do
+        defmodule Test do
+          @spec x()
+          @doc ""
+          def x()
+        end
+      end
+
+    assert DocSpecOrder.run(ast) == error
+
+    ast =
+      quote do
+        defmodule Test do
+          @spec x()
+          @doc ""
+          defp x()
+        end
+      end
+
+    assert DocSpecOrder.run(ast) == error
+
+    ast =
+      quote do
+        defmodule Test do
+          @spec x()
+          @doc ""
+          defmacro x()
+        end
+      end
+
+    assert DocSpecOrder.run(ast) == error
+
+    ast =
+      quote do
+        defmodule Test do
+          @spec x()
+          @doc ""
+          defmacrop x()
+        end
+      end
+
+    assert DocSpecOrder.run(ast) == error
+
+    ast =
+      quote do
+        defmodule Test do
+          @spec x()
+          @doc ""
+          defguard x()
+        end
+      end
+
+    assert DocSpecOrder.run(ast) == error
+
+    ast =
+      quote do
+        defmodule Test do
+          @spec x()
+          @doc ""
+          defguardp x()
+        end
+      end
+
+    assert DocSpecOrder.run(ast) == error
+  end
+
+  test "non related definitions will not make crash" do
+    ast =
+      quote do
+        @doc ""
+        def x
+
+        @spec y
+        def y
+      end
+
+    assert DocSpecOrder.run(ast) == []
+  end
+
+  test "multiple functions before attributes will not crash" do
+    ast =
+      quote do
+        def a
+        def b
+
+        @doc ""
+        @spec c
+        def c
+      end
+
+    assert DocSpecOrder.run(ast) == []
+  end
+
+  test "function definition order doesnt impact", %{error: error} do
+    ast =
+      quote do
+        defmodule Test do
+          def a
+          def b
+
+          @spec c
+          @doc ""
+          def c
+        end
+      end
+
+    assert DocSpecOrder.run(ast) == error
+  end
+
+  test "other modules attributes will not make it crash" do
+    ast =
+      quote do
+        defmodule Test do
+          @const "Const"
+
+          @doc ""
+          @spec x
+          def x
+
+          @answer 42
+
+          @doc ""
+          @spec y
+          def y
+        end
+      end
+
+    assert DocSpecOrder.run(ast) == []
   end
 end
