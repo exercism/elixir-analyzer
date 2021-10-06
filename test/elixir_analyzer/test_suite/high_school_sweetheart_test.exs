bf76ea81424900e567f0324daf38836a29612075
Luan Gomes <luan_saopaulo06@hotmail.com>
2021-10-05 22:31:29 -0300

    group tests

--- test/elixir_analyzer/test_suite/high_school_sweetheart_test.exs
+++ test/elixir_analyzer/test_suite/high_school_sweetheart_test.exs
@@ -30,19 +30,19 @@ defmodule ElixirAnalyzer.TestSuite.HighSchoolSweetheartTest do
         i2 = initials(full_name2)
 
         """
-              ******       ******
-            **      **   **      **
-          **         ** **         **
+             ******       ******
+           **      **   **      **
+         **         ** **         **
         **            *            **
         **                         **
         **     #{i1}  +  #{i2}     **
-          **                       **
-            **                   **
-              **               **
-                **           **
-                  **       **
-                    **   **
-                      ***
+         **                       **
+           **                   **
+             **               **
+               **           **
+                 **       **
+                   **   **
+                     ***
                       *
         """
       end
@@ -50,67 +50,77 @@ defmodule ElixirAnalyzer.TestSuite.HighSchoolSweetheartTest do
   end
 
   describe "function reuse" do
-    test_exercise_analysis "HighSchoolSweetheart.initial must call first_letter",
+    test_exercise_analysis "detects lack of reuse in all cases",
       comments_include: [Constants.high_school_sweetheart_function_reuse()] do
-      defmodule HighSchoolSweetheart do
-        def initial(name) do
-          name
-          |> String.trim()
-          |> String.first()
-          |> String.upcase()
-          |> Kernel.<>(".")
-        end
-      end
-    end
+        [
+          defmodule HighSchoolSweetheart do
+            def initial(name) do
+              name
+              |> String.trim()
+              |> String.first()
+              |> String.upcase()
+              |> Kernel.<>(".")
+            end
+          end,
+          defmodule HighSchoolSweetheart do
+            def initials(full_name) do
+              [first_name, last_name] = String.split(full_name)
 
-    test_exercise_analysis "HighSchoolSweetheart.initials must call initial",
-      comments_include: [Constants.high_school_sweetheart_function_reuse()] do
-      defmodule HighSchoolSweetheart do
-        def initials(full_name) do
-          [first_name, last_name] = String.split(full_name)
+              first_name =
+                first_name
+                |> first_letter()
+                |> String.upcase()
+                |> Kernel.<>(".")
 
-          first_name =
-            first_name
-            |> first_letter()
-            |> String.upcase()
-            |> Kernel.<>(".")
+              last_name =
+                last_name
+                |> first_letter()
+                |> String.upcase()
+                |> Kernel.<>(".")
 
-          last_name =
-            last_name
-            |> first_letter()
-            |> String.upcase()
-            |> Kernel.<>(".")
+              "#{first_name} #{last_name}"
+            end
+          end,
+          defmodule HighSchoolSweetheart do
+            def first_letter(name) do
+              name
+              |> String.trim()
+              |> String.first()
+            end
 
-          "#{first_name} #{last_name}"
-        end
-      end
-    end
+            def initial(name) do
+              name
+              |> first_letter()
+              |> String.upcase()
+              |> Kernel.<>(".")
+            end
 
-    test_exercise_analysis "HighSchoolSweetheart.pair must call initials",
-      comments_include: [Constants.high_school_sweetheart_function_reuse()] do
-      defmodule HighSchoolSweetheart do
-        def pair(full_name1, full_name2) do
-          i1 = full_name1
-          i2 = full_name2
+            def pair(full_name1, full_name2) do
+              [first_name, last_name] = String.split(full_name1)
+              i1 = "#{initial(first_name)} #{initial(last_name)}"
 
-          """
-                ******       ******
-              **      **   **      **
-            **         ** **         **
-          **            *            **
-          **                         **
-          **     #{i1}  +  #{i2}     **
-            **                       **
-              **                   **
-                **               **
-                  **           **
-                    **       **
-                      **   **
-                        ***
-                        *
-          """
-        end
-      end
+              [first_name, last_name] = String.split(full_name2)
+              i2 = "#{initial(first_name)} #{initial(last_name)}"
+
+              """
+                   ******       ******
+                 **      **   **      **
+               **         ** **         **
+              **            *            **
+              **                         **
+              **     #{i1}  +  #{i2}     **
+               **                       **
+                 **                   **
+                   **               **
+                     **           **
+                       **       **
+                         **   **
+                           ***
+                            *
+              """
+            end
+          end
+        ]
     end
   end
 end
