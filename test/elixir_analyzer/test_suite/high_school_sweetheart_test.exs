1fb500e265bdd4539d574f0a9d9d71901178c110
Luan Gomes <luan_saopaulo06@hotmail.com>
2021-10-06 09:33:31 -0300

    fix formatting

--- test/elixir_analyzer/test_suite/high_school_sweetheart_test.exs
+++ test/elixir_analyzer/test_suite/high_school_sweetheart_test.exs
@@ -54,75 +54,75 @@ defmodule ElixirAnalyzer.TestSuite.HighSchoolSweetheartTest do
   describe "function reuse" do
     test_exercise_analysis "detects lack of reuse in all cases",
       comments_include: [Constants.high_school_sweetheart_function_reuse()] do
-        [
-          defmodule HighSchoolSweetheart do
-            def initial(name) do
-              name
-              |> String.trim()
-              |> String.first()
-              |> String.upcase()
-              |> Kernel.<>(".")
-            end
-          end,
-          defmodule HighSchoolSweetheart do
-            def initials(full_name) do
-              [first_name, last_name] = String.split(full_name)
+      [
+        defmodule HighSchoolSweetheart do
+          def initial(name) do
+            name
+            |> String.trim()
+            |> String.first()
+            |> String.upcase()
+            |> Kernel.<>(".")
+          end
+        end,
+        defmodule HighSchoolSweetheart do
+          def initials(full_name) do
+            [first_name, last_name] = String.split(full_name)
 
-              first_name =
-                first_name
-                |> first_letter()
-                |> String.upcase()
-                |> Kernel.<>(".")
-
-              last_name =
-                last_name
-                |> first_letter()
-                |> String.upcase()
-                |> Kernel.<>(".")
-
-              "#{first_name} #{last_name}"
-            end
-          end,
-          defmodule HighSchoolSweetheart do
-            def first_letter(name) do
-              name
-              |> String.trim()
-              |> String.first()
-            end
-
-            def initial(name) do
-              name
+            first_name =
+              first_name
               |> first_letter()
               |> String.upcase()
               |> Kernel.<>(".")
-            end
 
-            def pair(full_name1, full_name2) do
-              [first_name, last_name] = String.split(full_name1)
-              i1 = "#{initial(first_name)} #{initial(last_name)}"
+            last_name =
+              last_name
+              |> first_letter()
+              |> String.upcase()
+              |> Kernel.<>(".")
 
-              [first_name, last_name] = String.split(full_name2)
-              i2 = "#{initial(first_name)} #{initial(last_name)}"
-
-              """
-                   ******       ******
-                 **      **   **      **
-               **         ** **         **
-              **            *            **
-              **                         **
-              **     #{i1}  +  #{i2}     **
-               **                       **
-                 **                   **
-                   **               **
-                     **           **
-                       **       **
-                         **   **
-                           ***
-                            *
-              """
-            end
+            "#{first_name} #{last_name}"
           end
-        ]
+        end,
+        defmodule HighSchoolSweetheart do
+          def first_letter(name) do
+            name
+            |> String.trim()
+            |> String.first()
+          end
+
+          def initial(name) do
+            name
+            |> first_letter()
+            |> String.upcase()
+            |> Kernel.<>(".")
+          end
+
+          def pair(full_name1, full_name2) do
+            [first_name, last_name] = String.split(full_name1)
+            i1 = "#{initial(first_name)} #{initial(last_name)}"
+
+            [first_name, last_name] = String.split(full_name2)
+            i2 = "#{initial(first_name)} #{initial(last_name)}"
+
+            """
+                 ******       ******
+               **      **   **      **
+             **         ** **         **
+            **            *            **
+            **                         **
+            **     #{i1}  +  #{i2}     **
+             **                       **
+               **                   **
+                 **               **
+                   **           **
+                     **       **
+                       **   **
+                         ***
+                          *
+            """
+          end
+        end
+      ]
     end
   end
 end
