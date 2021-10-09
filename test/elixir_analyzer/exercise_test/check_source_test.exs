7fbc051b7bbbd010062b0a0bc5993b2afbbcc4e1
Jérémie Gillet <jie.gillet@gmail.com>
2021-10-09 22:41:52 +0900

    Add unit tests for check_source

---
+++ test/elixir_analyzer/exercise_test/check_source_test.exs
@@ -0,0 +1,124 @@
+defmodule ElixirAnalyzer.ExerciseTest.CheckSourceTest do
+  use ElixirAnalyzer.ExerciseTestCase,
+    exercise_test_module: ElixirAnalyzer.Support.AnalyzerVerification.CheckSource
+
+  test_exercise_analysis "empty module",
+    comments: ["always return false", "didn't use multiline"] do
+    ~S"""
+    defmodule CheckSourceVerification do
+    end
+    """
+  end
+
+  test_exercise_analysis "contains integer litterals",
+    comments: [
+      "always return false",
+      "used integer litteral from ?a to ?z",
+      "didn't use multiline"
+    ] do
+    ~S"""
+    defmodule CheckSourceVerification do
+      def foo(x) do
+        case x do
+          97 -> "a"
+          98 -> "b"
+          _ -> "z"
+        end
+      end
+    end
+    """
+  end
+
+  test_exercise_analysis "contains integer but false positive",
+    comments: [
+      "always return false",
+      "used integer litteral from ?a to ?z",
+      "didn't use multiline"
+    ] do
+    ~S"""
+    defmodule CheckSourceVerification do
+      def best_version(game) do
+        case game do
+          "fifa" -> "fifa 98"
+          "tomb raider" -> "tomb raider II (1997)"
+          _ -> "goat simulator"
+        end
+      end
+    end
+    """
+  end
+
+  test_exercise_analysis "uses multiline strings",
+    comments: ["always return false"] do
+    [
+      ~S'''
+      defmodule CheckSourceVerification do
+        @moduledoc """
+        this module doesn't do much
+        """
+      end
+      ''',
+      ~S'''
+      defmodule CheckSourceVerification do
+        def foo do
+          """
+          all
+          you
+          need
+          is
+          love
+          """
+        end
+      end
+      ''',
+      ~S"""
+      defmodule CheckSourceVerification do
+        def foo do
+          '''
+          love
+          is
+          all
+          you
+          need
+          '''
+        end
+      end
+      """
+    ]
+  end
+
+  test_exercise_analysis "short module",
+    comments: ["always return false", "module is too short", "didn't use multiline"] do
+    ~S"""
+    defmodule C do
+    end
+    """
+  end
+
+  test_exercise_analysis "badly formatted modules",
+    comments: ["always return false", "didn't use multiline", "module is not formatted"] do
+    [
+      ~S"""
+      defmodule CheckSourceVerification do
+                                    def foo(), do: :ok
+      end
+      """,
+      ~S"""
+      defmodule CheckSourceVerification do
+
+
+
+      def foo, do: :ok
+
+      end
+      """,
+      ~S"""
+      defmodule    CheckSourceVerification     do
+      end
+      """,
+      ~S"""
+      defmodule CheckSourceVerification do end
+      """
+    ]
+  end
+end
---
+++ test/support/analyzer_verification/check_source.ex
@@ -0,0 +1,63 @@
+defmodule ElixirAnalyzer.Support.AnalyzerVerification.CheckSource do
+  @moduledoc """
+  This is an exercise analyzer extension module to test the check_soucre macro
+  """
+
+  use ElixirAnalyzer.ExerciseTest
+
+  check_source "always true" do
+    type :actionable
+    comment "always return true"
+
+    check(_source) do
+      true
+    end
+  end
+
+  check_source "always false" do
+    type :actionable
+    comment "always return false"
+
+    check(_source) do
+      false
+    end
+  end
+
+  check_source "finds integer litteral" do
+    type :actionable
+    comment "used integer litteral from ?a to ?z"
+
+    check(source) do
+      integers = Enum.map(?a..?z, &to_string/1)
+
+      not Enum.any?(integers, &String.contains?(source, &1))
+    end
+  end
+
+  check_source "finds use of multiline strings" do
+    type :actionable
+    comment "didn't use multiline"
+
+    check(source) do
+      String.contains?(source, ["\"\"\"", "\'\'\'"])
+    end
+  end
+
+  check_source "module is suitably long" do
+    type :actionable
+    comment "module is too short"
+
+    check(source) do
+      String.length(source) > 20
+    end
+  end
+
+  check_source "module is well formatted" do
+    type :actionable
+    comment "module is not formatted"
+
+    check(source) do
+      String.trim(source) == Code.format_string!(source) |> Enum.join()
+    end
+  end
+end
