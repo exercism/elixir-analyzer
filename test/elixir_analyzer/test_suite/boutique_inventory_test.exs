7c5a691b9496e21ccc27030b4b628e30bdbe68b6
Matheus Moreira <fmmatheus@gmail.com>
2021-10-19 17:02:53 -0300

    Add fail solutions for boutique-inventory analyzer

--- test/elixir_analyzer/test_suite/boutique_inventory_test.exs
+++ test/elixir_analyzer/test_suite/boutique_inventory_test.exs
@@ -4,51 +4,74 @@ defmodule ElixirAnalyzer.ExerciseTest.BoutiqueInventoryTest do
 
   test_exercise_analysis "example solution",
     comments: [ElixirAnalyzer.Constants.solution_same_as_exemplar()] do
-    [
-      defmodule BoutiqueInventory do
-        def sort_by_price(inventory) do
-          Enum.sort_by(inventory, fn item -> Map.get(item, :price) end)
-        end
-
-        def with_missing_price(inventory) do
-          Enum.filter(inventory, fn item -> Map.get(item, :price) == nil end)
-        end
-
-        def increase_quantity(item, count) do
-          Map.update(item, :quantity_by_size, %{}, fn quantity_by_size ->
-            quantity_by_size
-            |> Enum.map(fn {size, quantity} -> {size, quantity + count} end)
-            |> Enum.into(%{})
-          end)
-        end
-
-        def total_quantity(item) do
-          Enum.reduce(Map.get(item, :quantity_by_size), 0, fn {_size, quantity}, acc ->
-            acc + quantity
-          end)
-        end
+    defmodule BoutiqueInventory do
+      def sort_by_price(inventory) do
+        Enum.sort_by(inventory, fn item -> Map.get(item, :price) end)
       end
-    ]
+
+      def with_missing_price(inventory) do
+        Enum.filter(inventory, fn item -> Map.get(item, :price) == nil end)
+      end
+
+      def increase_quantity(item, count) do
+        Map.update(item, :quantity_by_size, %{}, fn quantity_by_size ->
+          quantity_by_size
+          |> Enum.map(fn {size, quantity} -> {size, quantity + count} end)
+          |> Enum.into(%{})
+        end)
+      end
+
+      def total_quantity(item) do
+        Enum.reduce(Map.get(item, :quantity_by_size), 0, fn {_size, quantity}, acc ->
+          acc + quantity
+        end)
+      end
+    end
   end
 
   test_exercise_analysis "correct solution using Enum.reject instead Enum.filter",
-    comments: [] do
-    [
+    comments_exclude: [
+      ElixirAnalyzer.Constants.boutique_inventory_use_enum_filter_or_enum_reject()
+    ] do
+    defmodule BoutiqueInventory do
+      def with_missing_price(inventory) do
+        Enum.reject(inventory, fn item -> Map.get(item, :price) != nil end)
+      end
+    end
+  end
+
+  describe "fail solutions" do
+    test_exercise_analysis "sort_by_price not using Enum.sort_by",
+      comments_include: [ElixirAnalyzer.Constants.boutique_inventory_use_enum_sort_by()] do
       defmodule BoutiqueInventory do
         def sort_by_price(inventory) do
-          Enum.sort_by(inventory, fn item -> Map.get(item, :price) end)
+          Enum.sort(inventory, &(&1.price <= &2.price))
         end
+      end
+    end
 
+    test_exercise_analysis "with_missing_price not using Enum.filter or Enum.reject",
+      comments_include: [
+        ElixirAnalyzer.Constants.boutique_inventory_use_enum_filter_or_enum_reject()
+      ] do
+      defmodule BoutiqueInventory do
         def with_missing_price(inventory) do
-          Enum.reject(inventory, fn item -> Map.get(item, :price) != nil end)
-        end
-
-        def total_quantity(item) do
-          Enum.reduce(Map.get(item, :quantity_by_size), 0, fn {_size, quantity}, acc ->
-            acc + quantity
+          Enum.reduce(inventory, [], fn item, acc ->
+            if is_nil(item.price), do: List.insert_at(acc, -1, item), else: acc
           end)
         end
       end
-    ]
+    end
+
+    test_exercise_analysis "total_quantity not using Enum.reduce",
+      comments_include: [ElixirAnalyzer.Constants.boutique_inventory_use_enum_reduce()] do
+      defmodule BoutiqueInventory do
+        def total_quantity(item) do
+          item[:quantity_by_size]
+          |> Map.values()
+          |> Enum.sum()
+        end
+      end
+    end
   end
 end
