03e2c8ae0ccac19b947868dfd6fc4a2d1308f1ec
Jérémie Gillet <jie.gillet@gmail.com>
2021-10-22 22:06:35 +0900

    Add tests for missing/corrupted example files

--- test/elixir_analyzer_test.exs
+++ test/elixir_analyzer_test.exs
@@ -24,53 +24,57 @@ defmodule ElixirAnalyzerTest do
     test "referred solution with comments" do
       exercise = "two-fer"
       path = "./test_data/two_fer/imperfect_solution/"
-      analyzed_exercise = ElixirAnalyzer.analyze_exercise(exercise, path, path, @options)
 
-      expected_output =
-        "{\"comments\":[{\"comment\":\"elixir.two-fer.use_of_function_header\",\"type\":\"actionable\"},{\"comment\":\"elixir.solution.use_specification\",\"type\":\"actionable\"},{\"comment\":\"elixir.solution.raise_fn_clause_error\",\"type\":\"actionable\"},{\"comment\":\"elixir.solution.variable_name_snake_case\",\"params\":{\"actual\":\"_nameInPascalCase\",\"expected\":\"_name_in_pascal_case\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.module_attribute_name_snake_case\",\"params\":{\"actual\":\"someUnusedModuleAttribute\",\"expected\":\"some_unused_module_attribute\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.module_pascal_case\",\"params\":{\"actual\":\"My_empty_module\",\"expected\":\"MyEmptyModule\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.compiler_warnings\",\"params\":{\"warnings\":\"warning: module attribute @someUnusedModuleAttribute was set but never used\\n  nofile:2\\n\\n\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.use_module_doc\",\"type\":\"informative\"},{\"comment\":\"elixir.solution.indentation\",\"type\":\"informative\"}],\"summary\":\"Check the comments for some code suggestions. 📣\"}"
+      assert capture_log(fn ->
+               analyzed_exercise = ElixirAnalyzer.analyze_exercise(exercise, path, path, @options)
 
-      assert Submission.to_json(analyzed_exercise) == expected_output
+               expected_output =
+                 "{\"comments\":[{\"comment\":\"elixir.two-fer.use_of_function_header\",\"type\":\"actionable\"},{\"comment\":\"elixir.solution.use_specification\",\"type\":\"actionable\"},{\"comment\":\"elixir.solution.raise_fn_clause_error\",\"type\":\"actionable\"},{\"comment\":\"elixir.solution.variable_name_snake_case\",\"params\":{\"actual\":\"_nameInPascalCase\",\"expected\":\"_name_in_pascal_case\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.module_attribute_name_snake_case\",\"params\":{\"actual\":\"someUnusedModuleAttribute\",\"expected\":\"some_unused_module_attribute\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.module_pascal_case\",\"params\":{\"actual\":\"My_empty_module\",\"expected\":\"MyEmptyModule\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.compiler_warnings\",\"params\":{\"warnings\":\"warning: module attribute @someUnusedModuleAttribute was set but never used\\n  nofile:2\\n\\n\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.use_module_doc\",\"type\":\"informative\"},{\"comment\":\"elixir.solution.indentation\",\"type\":\"informative\"}],\"summary\":\"Check the comments for some code suggestions. 📣\"}"
+
+               assert Submission.to_json(analyzed_exercise) == expected_output
+             end) =~ "Exemploid file not found. Reason: enoent"
     end
 
     test "error solution" do
       exercise = "two-fer"
       path = "./test_data/two_fer/error_solution/"
 
-      capture_log(fn ->
-        analyzed_exercise = ElixirAnalyzer.analyze_exercise(exercise, path, path, @options)
+      assert capture_log(fn ->
+               analyzed_exercise = ElixirAnalyzer.analyze_exercise(exercise, path, path, @options)
 
-        expected_output = """
-        {\"comments\":[{\"comment\":\"elixir.general.parsing_error\",\"params\":{\"error\":\"missing terminator: end (for \\\"do\\\" starting at line 1)\",\"line\":14},\"type\":\"essential\"}],\"summary\":\"Check the comments for things to fix. 🛠\"}
-        """
+               expected_output = """
+               {\"comments\":[{\"comment\":\"elixir.general.parsing_error\",\"params\":{\"error\":\"missing terminator: end (for \\\"do\\\" starting at line 1)\",\"line\":14},\"type\":\"essential\"}],\"summary\":\"Check the comments for things to fix. 🛠\"}
+               """
 
-        assert Submission.to_json(analyzed_exercise) == String.trim(expected_output)
-      end)
+               assert Submission.to_json(analyzed_exercise) == String.trim(expected_output)
+             end) =~ "Exemploid file could not be parsed."
     end
 
     test "missing file solution" do
       exercise = "two-fer"
       path = "./test_data/two_fer/missing_file_solution/"
-      analyzed_exercise = ElixirAnalyzer.analyze_exercise(exercise, path, path, @options)
 
-      expected_output = """
-      {\"comments\":[{\"comment\":\"elixir.general.file_not_found\",\"params\":{\"file_name\":\"two_fer.ex\",\"path\":\"./test_data/two_fer/missing_file_solution/\"},\"type\":\"essential\"}],\"summary\":\"Check the comments for things to fix. 🛠\"}
-      """
+      assert capture_log(fn ->
+               analyzed_exercise = ElixirAnalyzer.analyze_exercise(exercise, path, path, @options)
 
-      assert Submission.to_json(analyzed_exercise) == String.trim(expected_output)
+               expected_output = """
+               {\"comments\":[{\"comment\":\"elixir.general.file_not_found\",\"params\":{\"file_name\":\"two_fer.ex\",\"path\":\"./test_data/two_fer/missing_file_solution/\"},\"type\":\"essential\"}],\"summary\":\"Check the comments for things to fix. 🛠\"}
+               """
+
+               assert Submission.to_json(analyzed_exercise) == String.trim(expected_output)
+             end) =~ "Code file not found. Reason: enoent"
     end
 
     test "solution for an exercise with no analyzer module uses the default module" do
       exercise = "not-a-real-exercise"
       path = "./test_data/two_fer/imperfect_solution/"
 
-      capture_log(fn ->
-        analyzed_exercise = ElixirAnalyzer.analyze_exercise(exercise, path, path, @options)
+      analyzed_exercise = ElixirAnalyzer.analyze_exercise(exercise, path, path, @options)
 
-        expected_output =
-          "{\"comments\":[{\"comment\":\"elixir.solution.raise_fn_clause_error\",\"type\":\"actionable\"},{\"comment\":\"elixir.solution.variable_name_snake_case\",\"params\":{\"actual\":\"_nameInPascalCase\",\"expected\":\"_name_in_pascal_case\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.module_attribute_name_snake_case\",\"params\":{\"actual\":\"someUnusedModuleAttribute\",\"expected\":\"some_unused_module_attribute\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.module_pascal_case\",\"params\":{\"actual\":\"My_empty_module\",\"expected\":\"MyEmptyModule\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.compiler_warnings\",\"params\":{\"warnings\":\"warning: module attribute @someUnusedModuleAttribute was set but never used\\n  nofile:2\\n\\n\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.indentation\",\"type\":\"informative\"}],\"summary\":\"Check the comments for some code suggestions. 📣\"}"
+      expected_output =
+        "{\"comments\":[{\"comment\":\"elixir.solution.raise_fn_clause_error\",\"type\":\"actionable\"},{\"comment\":\"elixir.solution.variable_name_snake_case\",\"params\":{\"actual\":\"_nameInPascalCase\",\"expected\":\"_name_in_pascal_case\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.module_attribute_name_snake_case\",\"params\":{\"actual\":\"someUnusedModuleAttribute\",\"expected\":\"some_unused_module_attribute\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.module_pascal_case\",\"params\":{\"actual\":\"My_empty_module\",\"expected\":\"MyEmptyModule\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.compiler_warnings\",\"params\":{\"warnings\":\"warning: module attribute @someUnusedModuleAttribute was set but never used\\n  nofile:2\\n\\n\"},\"type\":\"actionable\"},{\"comment\":\"elixir.solution.indentation\",\"type\":\"informative\"}],\"summary\":\"Check the comments for some code suggestions. 📣\"}"
 
-        assert Submission.to_json(analyzed_exercise) == String.trim(expected_output)
-      end)
+      assert Submission.to_json(analyzed_exercise) == String.trim(expected_output)
     end
   end
 
@@ -110,7 +114,7 @@ defmodule ElixirAnalyzerTest do
                  "{\"comments\":[],\"summary\":\"Submission analyzed. No automated suggestions found.\"}"
 
                assert Submission.to_json(analyzed_exercise) == String.trim(expected_output)
-             end) =~ "Exemplar file not found. Reason: enoent"
+             end) =~ "Exemploid file not found. Reason: enoent"
     end
 
     test "solution with parsing error for incomplete exemplar" do
@@ -124,7 +128,7 @@ defmodule ElixirAnalyzerTest do
                  "{\"comments\":[],\"summary\":\"Submission analyzed. No automated suggestions found.\"}"
 
                assert Submission.to_json(analyzed_exercise) == String.trim(expected_output)
-             end) =~ "Exemplar file could not be parsed."
+             end) =~ "Exemploid file could not be parsed."
     end
   end
 
