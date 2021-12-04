c9280da07a33aa21a04111c3e998cb52b133a102
Ryan Zidago <ryanzidago@gmail.com>
2021-12-04 18:03:12 +0100

    do not trigger a warning for Enum.reduce when testing if Kernel.SpecialForms.for/1 is used or not

--- test/elixir_analyzer/test_suite/dna_encoding_test.exs
+++ test/elixir_analyzer/test_suite/dna_encoding_test.exs
@@ -134,16 +134,17 @@ defmodule ElixirAnalyzer.ExerciseTest.DNATest do
         def encode_nucleotide(?C), do: 0b0010
         def encode_nucleotide(?G), do: 0b0100
         def encode_nucleotide(?T), do: 0b1000
+
         def decode_nucleotide(0b0000), do: ?\s
         def decode_nucleotide(0b0001), do: ?A
         def decode_nucleotide(0b0010), do: ?C
         def decode_nucleotide(0b0100), do: ?G
         def decode_nucleotide(0b1000), do: ?T
 
-        def encode(dna) do
-          dna
-          |> Enum.map(&encode_nucleotide/1)
-          |> Enum.reduce("", fn enc, bs -> <<bs::bitstring, enc::4>> end)
+        def encode([]), do: <<>>
+
+        def encode([nucleotide | dna]) do
+          <<encode_nucleotide(nucleotide)::4, encode(dna)::bitstring()>>
         end
 
         def decode(dna) do
