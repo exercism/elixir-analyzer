d8d7d40b4ccce68b18b3112fd4bb2022518c51bc
Ryan Zidago <ryanzidago@gmail.com>
2021-12-06 22:29:54 +0100

    extended check to Enum, List, Stream and list comprehensions

--- lib/elixir_analyzer/test_suite/dna_encoding.ex
+++ lib/elixir_analyzer/test_suite/dna_encoding.ex
@@ -6,19 +6,25 @@ defmodule ElixirAnalyzer.TestSuite.DNAEncoding do
   use ElixirAnalyzer.ExerciseTest
   alias ElixirAnalyzer.Constants
 
-  assert_no_call "does not call Enum.reduce" do
+  assert_no_call "does not call any Enum functions" do
     type :essential
-    called_fn module: Enum, name: :reduce
+    called_fn module: Enum, name: :_
     comment Constants.dna_encoding_use_recursion()
   end
 
-  assert_no_call "does not call List.foldr" do
+  assert_no_call "does not call any Stream functions" do
     type :essential
-    called_fn module: List, name: :foldr
+    called_fn module: Stream, name: :_
     comment Constants.dna_encoding_use_recursion()
   end
 
-  assert_no_call "does not call the for/1 function" do
+  assert_no_call "does not call any List functions" do
+    type :essential
+    called_fn module: List, name: :_
+    comment Constants.dna_encoding_use_recursion()
+  end
+
+  assert_no_call "does not use list comprehensions" do
     type :essential
     called_fn name: :for
     comment Constants.dna_encoding_use_recursion()
--- test/elixir_analyzer/test_suite/dna_encoding_test.exs
+++ test/elixir_analyzer/test_suite/dna_encoding_test.exs
@@ -56,7 +56,7 @@ defmodule ElixirAnalyzer.ExerciseTest.DNAEncodingTest do
     end
   end
 
-  test_exercise_analysis "detecs Enum.reduce",
+  test_exercise_analysis "detects the usage of the Enum module",
     comments: [Constants.dna_encoding_use_recursion()] do
     [
       defmodule DNA do
@@ -65,6 +65,7 @@ defmodule ElixirAnalyzer.ExerciseTest.DNAEncodingTest do
         def encode_nucleotide(?C), do: 0b0010
         def encode_nucleotide(?G), do: 0b0100
         def encode_nucleotide(?T), do: 0b1000
+
         def decode_nucleotide(0b0000), do: ?\s
         def decode_nucleotide(0b0001), do: ?A
         def decode_nucleotide(0b0010), do: ?C
@@ -78,49 +79,53 @@ defmodule ElixirAnalyzer.ExerciseTest.DNAEncodingTest do
         end
 
         def decode(dna) do
-          for <<ns::4 <- dna>> do
-            decode_nucleotide(ns)
-          end
+          do_decode(dna, [])
         end
+
+        defp do_decode(<<>>, acc), do: acc |> reverse()
+
+        defp do_decode(<<n::4, rest::bitstring>>, acc),
+          do: do_decode(rest, [decode_nucleotide(n) | acc])
+
+        defp reverse(l), do: do_reverse(l, [])
+        defp do_reverse([], acc), do: acc
+        defp do_reverse([h | t], acc), do: do_reverse(t, [h | acc])
       end
     ]
   end
 
-  test_exercise_analysis "detects List.foldr",
-    comments: [Constants.dna_encoding_use_recursion(), Constants.solution_use_function_capture()] do
+  test_exercise_analysis "detects the usage of the List module",
+    comments: [Constants.dna_encoding_use_recursion()] do
     [
       defmodule DNA do
-        @acids %{
-          ?\s => 0b0000,
-          ?A => 0b0001,
-          ?C => 0b0010,
-          ?G => 0b0100,
-          ?T => 0b1000
-        }
+        def encode_nucleotide(?\s), do: 0b0000
+        def encode_nucleotide(?A), do: 0b0001
+        def encode_nucleotide(?C), do: 0b0010
+        def encode_nucleotide(?G), do: 0b0100
+        def encode_nucleotide(?T), do: 0b1000
 
-        def encode_nucleotide(code_point), do: @acids[code_point]
-
-        def decode_nucleotide(encoded_code) do
-          {key, _} = Enum.find(@acids, nil, fn {_, code} -> code == encoded_code end)
-          key
-        end
+        def decode_nucleotide(0b0000), do: ?\s
+        def decode_nucleotide(0b0001), do: ?A
+        def decode_nucleotide(0b0010), do: ?C
+        def decode_nucleotide(0b0100), do: ?G
+        def decode_nucleotide(0b1000), do: ?T
 
         def encode(dna) do
-          Enum.map(dna, &encode_nucleotide(&1))
-          |> List.foldr(<<0::0>>, &<<&1::4, &2::bitstring>>)
+          List.foldr(dna, <<0::0>>, &<<encode_nucleotide(&1)::4, &2::bitstring>>)
         end
 
         def decode(dna) do
           do_decode(dna, [])
-          |> Enum.reverse()
         end
 
-        defp do_decode(<<0::0>>, acc), do: acc
+        defp do_decode(<<>>, acc), do: acc |> reverse()
 
-        defp do_decode(dna, acc) do
-          <<value::4, rest::bitstring>> = dna
-          do_decode(rest, [decode_nucleotide(value) | acc])
-        end
+        defp do_decode(<<n::4, rest::bitstring>>, acc),
+          do: do_decode(rest, [decode_nucleotide(n) | acc])
+
+        defp reverse(l), do: do_reverse(l, [])
+        defp do_reverse([], acc), do: acc
+        defp do_reverse([h | t], acc), do: do_reverse(t, [h | acc])
       end
     ]
   end
@@ -141,10 +146,14 @@ defmodule ElixirAnalyzer.ExerciseTest.DNAEncodingTest do
         def decode_nucleotide(0b0100), do: ?G
         def decode_nucleotide(0b1000), do: ?T
 
-        def encode([]), do: <<>>
+        def encode(dna) do
+          do_encode(dna, <<>>)
+        end
 
-        def encode([nucleotide | dna]) do
-          <<encode_nucleotide(nucleotide)::4, encode(dna)::bitstring()>>
+        defp do_encode([], acc), do: acc
+
+        defp do_encode([n | rest], acc) do
+          do_encode(rest, <<acc::bitstring, encode_nucleotide(n)::4>>)
         end
 
         def decode(dna) do
