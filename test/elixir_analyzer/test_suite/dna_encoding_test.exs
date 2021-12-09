088f46930d10bbdac9855b113ed5685080602772
Ryan Zidago <ryanzidago@gmail.com>
2021-12-09 21:18:35 +0100

    added test case for the Stream module

--- test/elixir_analyzer/test_suite/dna_encoding_test.exs
+++ test/elixir_analyzer/test_suite/dna_encoding_test.exs
@@ -94,6 +94,42 @@ defmodule ElixirAnalyzer.ExerciseTest.DNAEncodingTest do
     ]
   end
 
+  test_exercise_analysis "detects the usage of the Stream module",
+    comments: [Constants.dna_encoding_use_recursion()] do
+    [
+      defmodule DNA do
+        def encode_nucleotide(?\s), do: 0b0000
+        def encode_nucleotide(?A), do: 0b0001
+        def encode_nucleotide(?C), do: 0b0010
+        def encode_nucleotide(?G), do: 0b0100
+        def encode_nucleotide(?T), do: 0b1000
+
+        def decode_nucleotide(0b0000), do: ?\s
+        def decode_nucleotide(0b0001), do: ?A
+        def decode_nucleotide(0b0010), do: ?C
+        def decode_nucleotide(0b0100), do: ?G
+        def decode_nucleotide(0b1000), do: ?T
+
+        def encode(dna) do
+          Stream.map(dna, &encode_nucleotide/1)
+        end
+
+        def decode(dna) do
+          do_decode(dna, [])
+        end
+
+        defp do_decode(<<>>, acc), do: acc |> reverse()
+
+        defp do_decode(<<n::4, rest::bitstring>>, acc),
+          do: do_decode(rest, [decode_nucleotide(n) | acc])
+
+        defp reverse(l), do: do_reverse(l, [])
+        defp do_reverse([], acc), do: acc
+        defp do_reverse([h | t], acc), do: do_reverse(t, [h | acc])
+      end
+    ]
+  end
+
   test_exercise_analysis "detects the usage of the List module",
     comments: [Constants.dna_encoding_use_recursion()] do
     [
