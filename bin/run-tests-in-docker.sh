71158936f045850e83db3984ef500bd907978727
Jérémie Gillet <jie.gillet@gmail.com>
2022-03-06 11:02:42 +0900

    match read-only production flag

--- bin/run-tests-in-docker.sh
+++ bin/run-tests-in-docker.sh
@@ -18,11 +18,14 @@ set -u # Exit on usage of undeclared variable.
 set -o pipefail # Catch failures in pipes.
 
 # build docker image
-docker build -t elixir-analyzer .
+docker build --rm -t elixir-analyzer .
 
 # run image passing the arguments
 docker run \
+    --rm \
     --network none \
+    --read-only \
     --mount type=bind,src=$(realpath test_data),dst=/opt/analyzer/test_data \
+    --mount type=tmpfs,dst=/tmp \
     --entrypoint /opt/analyzer/bin/smoke_test.sh \
     elixir-analyzer
