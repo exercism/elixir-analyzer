453b204002553b3ebd3a08e66b91ef50ae538e04
Angelika Tyborska <angelikatyborska@fastmail.com>
2022-04-12 07:43:04 +0200

    Add `@impl` test

--- test/elixir_analyzer/test_suite/take_a_number_deluxe_test.exs
+++ test/elixir_analyzer/test_suite/take_a_number_deluxe_test.exs
@@ -123,4 +123,97 @@ defmodule ElixirAnalyzer.TestSuite.TakeANumberDeluxeTest do
       end
     end
   end
+
+  describe "must annotate with `@impl GenServer`" do
+    test_exercise_analysis "forgot `@impl` entirely",
+      comments_include: [Constants.take_a_number_deluxe_annotate_impl_genserver()] do
+      defmodule TakeANumberDeluxe do
+        use GenServer
+
+        def init(init_arg) do
+          min_number = Keyword.get(init_arg, :min_number)
+          max_number = Keyword.get(init_arg, :max_number)
+          auto_shutdown_timeout = Keyword.get(init_arg, :auto_shutdown_timeout, :infinity)
+
+          case TakeANumberDeluxe.State.new(min_number, max_number, auto_shutdown_timeout) do
+            {:ok, state} -> {:ok, state, auto_shutdown_timeout}
+            {:error, error} -> {:stop, error}
+          end
+        end
+
+        def handle_call(:report_state, _from, state) do
+          {:reply, state, state, state.auto_shutdown_timeout}
+        end
+      end
+    end
+
+    test_exercise_analysis "value must be a module",
+      comments_include: [Constants.take_a_number_deluxe_annotate_impl_genserver()] do
+      defmodule TakeANumberDeluxe do
+        use GenServer
+
+        @impl true
+        def init(init_arg) do
+          min_number = Keyword.get(init_arg, :min_number)
+          max_number = Keyword.get(init_arg, :max_number)
+          auto_shutdown_timeout = Keyword.get(init_arg, :auto_shutdown_timeout, :infinity)
+
+          case TakeANumberDeluxe.State.new(min_number, max_number, auto_shutdown_timeout) do
+            {:ok, state} -> {:ok, state, auto_shutdown_timeout}
+            {:error, error} -> {:stop, error}
+          end
+        end
+
+        @impl true
+        def handle_call(:report_state, _from, state) do
+          {:reply, state, state, state.auto_shutdown_timeout}
+        end
+      end
+    end
+
+    test_exercise_analysis "used at least once is enough",
+      comments_exclude: [Constants.take_a_number_deluxe_annotate_impl_genserver()] do
+      [
+        defmodule TakeANumberDeluxe do
+          use GenServer
+
+          @impl GenServer
+          def init(init_arg) do
+            min_number = Keyword.get(init_arg, :min_number)
+            max_number = Keyword.get(init_arg, :max_number)
+            auto_shutdown_timeout = Keyword.get(init_arg, :auto_shutdown_timeout, :infinity)
+
+            case TakeANumberDeluxe.State.new(min_number, max_number, auto_shutdown_timeout) do
+              {:ok, state} -> {:ok, state, auto_shutdown_timeout}
+              {:error, error} -> {:stop, error}
+            end
+          end
+
+          @impl GenServer
+          def handle_call(:report_state, _from, state) do
+            {:reply, state, state, state.auto_shutdown_timeout}
+          end
+        end,
+        defmodule TakeANumberDeluxe do
+          use GenServer
+
+          def init(init_arg) do
+            min_number = Keyword.get(init_arg, :min_number)
+            max_number = Keyword.get(init_arg, :max_number)
+            auto_shutdown_timeout = Keyword.get(init_arg, :auto_shutdown_timeout, :infinity)
+
+            case TakeANumberDeluxe.State.new(min_number, max_number, auto_shutdown_timeout) do
+              {:ok, state} -> {:ok, state, auto_shutdown_timeout}
+              {:error, error} -> {:stop, error}
+            end
+          end
+
+          @impl GenServer
+          def handle_call(:report_state, _from, state) do
+            {:reply, state, state, state.auto_shutdown_timeout}
+          end
+        end
+      ]
+    end
+  end
 end
