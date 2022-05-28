d8154006d3ebfdfefe6ede41710df1a7ee77baa0
Angelika Tyborska <angelikatyborska@fastmail.com>
2022-05-28 15:45:36 +0200

    Add one more good example

--- test/elixir_analyzer/test_suite/basketball_website_test.exs
+++ test/elixir_analyzer/test_suite/basketball_website_test.exs
@@ -43,6 +43,18 @@ defmodule ElixirAnalyzer.ExerciseTest.BasketballWebsiteTest do
         end
       end
     end
+
+    test_exercise_analysis "using Enum is also fine", comments: [] do
+      defmodule BasketballWebsite do
+        def extract_from_path(data, path) do
+          Enum.reduce(String.split(path, "."), data, fn k, acc -> acc[k] end)
+        end
+
+        def get_in_path(data, path) do
+          get_in(data, String.split(path, "."))
+        end
+      end
+    end
   end
 
   describe "explicit nil" do
