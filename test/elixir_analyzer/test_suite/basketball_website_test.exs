9279a3a4e766f9f56d292ee53d012f96ef6fb0fd
Fatih Altuntaş <fatih.altuntas@trendyol.com>
2022-07-15 17:35:00 +0300

    Remove no explicit nil handling feedback from basketball_website

--- test/elixir_analyzer/test_suite/basketball_website_test.exs
+++ test/elixir_analyzer/test_suite/basketball_website_test.exs
@@ -57,25 +57,6 @@ defmodule ElixirAnalyzer.ExerciseTest.BasketballWebsiteTest do
     end
   end
 
-  describe "explicit nil" do
-    test_exercise_analysis "there should be no explicit nil handling",
-      comments_include: [ElixirAnalyzer.Constants.basketball_website_no_explicit_nil()] do
-      defmodule BasketballWebsite do
-        def extract_from_path(data, path) do
-          paths = String.split(path, ".", trim: true)
-          do_extract(data, paths)
-        end
-
-        defp do_extract(nil, _), do: nil
-        defp do_extract(data, []), do: data
-
-        defp do_extract(data, [path | next]) do
-          do_extract(data[path], next)
-        end
-      end
-    end
-  end
-
   describe "using Map" do
     test_exercise_analysis "there should be no usage of the map module",
       comments_include: [ElixirAnalyzer.Constants.basketball_website_no_explicit_nil()] do
