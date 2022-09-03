813274fe81713145f3d05fc1eda0e723b325f57d
Jie <jie.gillet@gmail.com>
2022-09-04 01:47:18 +0900

    Allow multiple suppress_if in all macros (#329)

--- lib/elixir_analyzer/comment.ex
+++ lib/elixir_analyzer/comment.ex
@@ -4,13 +4,13 @@ defmodule ElixirAnalyzer.Comment do
   (see https://github.com/exercism/docs/blob/main/building/tooling/analyzers/interface.md#comments)
   """
 
-  defstruct status: :test, name: nil, comment: nil, type: nil, suppress_if: false, params: nil
+  defstruct status: :test, name: nil, comment: nil, type: nil, suppress_if: [], params: nil
 
   @type t :: %__MODULE__{
           name: String.t(),
           comment: String.t(),
           type: :essential | :actionable | :informative | :celebratory,
-          suppress_if: false | {String.t(), :pass | :fail},
+          suppress_if: [{String.t(), :pass | :fail}],
           params: map() | nil
         }
 
