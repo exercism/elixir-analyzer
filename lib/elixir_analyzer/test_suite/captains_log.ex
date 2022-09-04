3a9fb2849e8f553e39aaccd765eddfc703565d36
Angelika Tyborska <angelikatyborska@fastmail.com>
2022-09-04 07:29:00 +0200

    Separate "use" and "do not use" comments in captains-log (#328)
    * Separate "use" and "do not use" comments in captains-log
    
    * Mark future refactoring
    
    * Update file-sniffer exemplar
    
    * Update submodule
    
    * Make use of multiple suppress_if

--- lib/elixir_analyzer/test_suite/captains_log.ex
+++ lib/elixir_analyzer/test_suite/captains_log.ex
@@ -23,7 +23,14 @@ defmodule ElixirAnalyzer.TestSuite.CaptainsLog do
     type :essential
     calling_fn module: CaptainsLog, name: :random_stardate
     called_fn module: Enum, name: :random
-    comment Constants.captains_log_use_rand_uniform()
+    comment Constants.captains_log_do_not_use_enum_random()
+  end
+
+  assert_no_call "random_stardate does not use :rand.uniform_real" do
+    type :essential
+    calling_fn module: CaptainsLog, name: :random_stardate
+    called_fn module: :rand, name: :uniform_real
+    comment Constants.captains_log_do_not_use_rand_uniform_real()
   end
 
   assert_call "random_stardate uses :rand.uniform" do
@@ -32,6 +39,8 @@ defmodule ElixirAnalyzer.TestSuite.CaptainsLog do
     called_fn module: :rand, name: :uniform
     comment Constants.captains_log_use_rand_uniform()
     suppress_if Constants.solution_deprecated_random_module(), :fail
+    suppress_if "random_stardate does not use :rand.uniform_real", :fail
+    suppress_if "random_stardate does not use Enum.random", :fail
   end
 
   assert_call "format_stardate uses :io_lib" do
