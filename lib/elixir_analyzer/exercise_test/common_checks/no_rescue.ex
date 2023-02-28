e25205b24a5e3bb15759014953266c892ef7bbc1
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-02-28 20:57:39 +0100

    Do not use a 1000 lines long quotes

--- lib/elixir_analyzer/exercise_test/common_checks/no_rescue.ex
+++ lib/elixir_analyzer/exercise_test/common_checks/no_rescue.ex
@@ -1,178 +1,33 @@
-# credo:disable-for-this-file Credo.Check.Refactor.LongQuoteBlocks
-
 defmodule ElixirAnalyzer.ExerciseTest.CommonChecks.NoRescue do
   @moduledoc """
   This is an exercise analyzer extension module used for common tests looking for the usage of `rescue`
   """
 
   alias ElixirAnalyzer.Constants
+  alias ElixirAnalyzer.Source
 
   defmacro __using__(_opts) do
     quote do
-      feature Constants.solution_no_rescue() do
+      check_source Constants.solution_no_rescue() do
         type :essential
         comment Constants.solution_no_rescue()
-        find :none
 
-        # try ------------------------------------------------------------------------
-        form do
-          try do
-            _ignore
-          rescue
-            _ignore
-          end
-        end
+        check(%Source{code_ast: code_ast}) do
+          {_, found} =
+            Macro.prewalk(code_ast, false, fn node, found ->
+              case node do
+                {def_op, _, [_function, [{:do, _} | rest]]} when def_op in [:def, :defp] ->
+                  {node, found or List.keymember?(rest, :rescue, 0)}
 
-        form do
-          try do
-            _ignore
-          rescue
-            _ignore
-          else
-            _ignore
-          end
-        end
+                {:try, _, [[{:do, _} | rest]]} ->
+                  {node, found or List.keymember?(rest, :rescue, 0)}
 
-        form do
-          try do
-            _ignore
-          rescue
-            _ignore
-          after
-            _ignore
-          end
-        end
+                _ ->
+                  {node, found}
+              end
+            end)
 
-        form do
-          try do
-            _ignore
-          rescue
-            _ignore
-          else
-            _ignore
-          after
-            _ignore
-          end
-        end
-
-        form do
-          try do
-            _ignore
-          rescue
-            _ignore
-          after
-            _ignore
-          else
-            _ignore
-          end
-        end
-
-        # def ------------------------------------------------------------------------
-
-        form do
-          def _ignore do
-            _ignore
-          rescue
-            _ignore
-          end
-        end
-
-        form do
-          def _ignore do
-            _ignore
-          rescue
-            _ignore
-          else
-            _ignore
-          end
-        end
-
-        form do
-          def _ignore do
-            _ignore
-          rescue
-            _ignore
-          after
-            _ignore
-          end
-        end
-
-        form do
-          def _ignore do
-            _ignore
-          rescue
-            _ignore
-          else
-            _ignore
-          after
-            _ignore
-          end
-        end
-
-        form do
-          def _ignore do
-            _ignore
-          rescue
-            _ignore
-          after
-            _ignore
-          else
-            _ignore
-          end
-        end
-
-        # defp -----------------------------------------------------------------------
-
-        form do
-          defp _ignore do
-            _ignore
-          rescue
-            _ignore
-          end
-        end
-
-        form do
-          defp _ignore do
-            _ignore
-          rescue
-            _ignore
-          else
-            _ignore
-          end
-        end
-
-        form do
-          defp _ignore do
-            _ignore
-          rescue
-            _ignore
-          after
-            _ignore
-          end
-        end
-
-        form do
-          defp _ignore do
-            _ignore
-          rescue
-            _ignore
-          else
-            _ignore
-          after
-            _ignore
-          end
-        end
-
-        form do
-          defp _ignore do
-            _ignore
-          rescue
-            _ignore
-          after
-            _ignore
-          else
-            _ignore
-          end
+          not found
         end
       end
     end
