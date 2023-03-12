a00fba76d410603c884055d5c02f617af15c7fa0
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-03-12 13:27:36 +0100

    Require tail-call recursion in dna-encoding

--- lib/elixir_analyzer/test_suite/dna_encoding.ex
+++ lib/elixir_analyzer/test_suite/dna_encoding.ex
@@ -5,6 +5,7 @@ defmodule ElixirAnalyzer.TestSuite.DNAEncoding do
 
   use ElixirAnalyzer.ExerciseTest
   alias ElixirAnalyzer.Constants
+  alias ElixirAnalyzer.Source
 
   assert_no_call "does not call any Enum functions" do
     type :essential
@@ -28,5 +29,114 @@ defmodule ElixirAnalyzer.TestSuite.DNAEncoding do
     type :essential
     called_fn name: :for
     comment Constants.dna_encoding_use_recursion()
+  end
+
+  check_source "uses tail call recursion" do
+    type :essential
+    suppress_if "does not call any Enum functions", :fail
+    suppress_if "does not call any Stream functions", :fail
+    suppress_if "does not call any List functions", :fail
+    suppress_if "does not use list comprehensions", :fail
+    comment Constants.dna_encoding_use_tail_call_recursion()
+
+    check(%Source{code_ast: code_ast}) do
+      {_, tail_call_recursive_functions} =
+        Macro.prewalk(
+          code_ast,
+          [],
+          fn node, acc -> find_tail_call_recursive_functions(node, acc) end
+        )
+
+      tail_call_recursive_functions = Enum.uniq(tail_call_recursive_functions)
+
+      {_, all_recursive_functions} =
+        Macro.prewalk(
+          code_ast,
+          [],
+          fn node, acc -> find_all_recursive_functions(node, acc) end
+        )
+
+      all_recursive_functions = Enum.uniq(all_recursive_functions)
+
+      non_tail_call_recursive_functions = all_recursive_functions -- tail_call_recursive_functions
+
+      if non_tail_call_recursive_functions != [] || tail_call_recursive_functions == [] do
+        false
+      else
+        true
+      end
+    end
+  end
+
+  defp find_tail_call_recursive_functions(node, acc) do
+    acc =
+      case node do
+        {op, _meta1, [{fn_name, _meta2, args}, opts]} when op in [:def, :defp] ->
+          fn_arity = length(args)
+
+          last_call_in_function_def =
+            case opts[:do] do
+              {:__block, _, calls} when is_list(calls) ->
+                List.last(calls)
+
+              calls when is_list(calls) ->
+                List.last(calls)
+
+              call ->
+                call
+            end
+
+          case last_call_in_function_def do
+            {^fn_name, _meta3, args} when length(args) == fn_arity ->
+              [{fn_name, fn_arity} | acc]
+
+            _ ->
+              acc
+          end
+
+        _ ->
+          acc
+      end
+
+    {node, acc}
+  end
+
+  defp find_all_recursive_functions(node, acc) do
+    acc =
+      case node do
+        {op, _meta1, [{fn_name, _meta2, args}, opts]} when op in [:def, :defp] ->
+          fn_arity = length(args)
+
+          all_calls_in_function_def =
+            case opts[:do] do
+              {:__block, _, calls} ->
+                calls
+
+              calls when is_list(calls) ->
+                calls
+
+              call ->
+                [call]
+            end
+
+          {_, any_nested_recursive_calls?} =
+            Macro.prewalk(all_calls_in_function_def, false, fn node, acc ->
+              case node do
+                {^fn_name, _, args} when length(args) == fn_arity -> {node, true}
+                _ -> {node, acc}
+              end
+            end)
+
+          if any_nested_recursive_calls? do
+            [{fn_name, fn_arity} | acc]
+          else
+            acc
+          end
+
+        _ ->
+          acc
+      end
+
+    {node, acc}
   end
 end
--- test/elixir_analyzer/test_suite/dna_encoding_test.exs
+++ test/elixir_analyzer/test_suite/dna_encoding_test.exs
@@ -209,4 +209,146 @@ defmodule ElixirAnalyzer.ExerciseTest.DNAEncodingTest do
       end
     ]
   end
+
+  test_exercise_analysis "recursive solutions but no tail call recursion",
+    comments: [ElixirAnalyzer.Constants.dna_encoding_use_tail_call_recursion()] do
+    [
+      defmodule DNA do
+        @nucleotide_encode_table %{
+          ?\s => 0b0000,
+          ?A => 0b0001,
+          ?C => 0b0010,
+          ?G => 0b0100,
+          ?T => 0b1000
+        }
+
+        @nucleotide_decode_table %{
+          0b0000 => ?\s,
+          0b0001 => ?A,
+          0b0010 => ?C,
+          0b0100 => ?G,
+          0b1000 => ?T
+        }
+
+        def encode_nucleotide(code_point) do
+          Map.fetch!(@nucleotide_encode_table, code_point)
+        end
+
+        def decode_nucleotide(encoded_code) do
+          Map.fetch!(@nucleotide_decode_table, encoded_code)
+        end
+
+        def encode(dna)
+        def encode([]), do: <<>>
+
+        def encode([head | tail]) do
+          <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
+        end
+
+        def decode(dna)
+        def decode(<<>>), do: []
+
+        def decode(<<head::4, tail::bitstring>>) do
+          [decode_nucleotide(head) | decode(tail)]
+        end
+      end,
+      defmodule DNA do
+        @moduledoc false
+
+        @type nucleotide_char :: ?\s | ?A | ?C | ?G | ?T
+        @type nucleotide_bitstring :: 0 | 1 | 2 | 4 | 8
+        @type dna_charlist :: [nucleotide_char()]
+        @type dna_bitstring :: <<_::_*4>>
+
+        @spec encode_nucleotide(nucleotide_char()) :: nucleotide_bitstring()
+        def encode_nucleotide(?\s), do: 0b0000
+        def encode_nucleotide(?A), do: 0b0001
+        def encode_nucleotide(?C), do: 0b0010
+        def encode_nucleotide(?G), do: 0b0100
+        def encode_nucleotide(?T), do: 0b1000
+
+        @spec decode_nucleotide(nucleotide_bitstring()) :: nucleotide_char()
+        def decode_nucleotide(0b0000), do: ?\s
+        def decode_nucleotide(0b0001), do: ?A
+        def decode_nucleotide(0b0010), do: ?C
+        def decode_nucleotide(0b0100), do: ?G
+        def decode_nucleotide(0b1000), do: ?T
+
+        @spec encode(dna_charlist()) :: dna_bitstring()
+        def encode([]), do: <<>>
+        def encode([head | tail]), do: <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
+
+        @spec decode(bitstring) :: dna_charlist()
+        def decode(<<>>), do: []
+        def decode(<<head::4, rest::bitstring>>), do: [decode_nucleotide(head) | decode(rest)]
+      end,
+      defmodule DNA do
+        @moduledoc false
+
+        @type nucleotide_char :: ?\s | ?A | ?C | ?G | ?T
+        @type nucleotide_bitstring :: 0 | 1 | 2 | 4 | 8
+        @type dna_charlist :: [nucleotide_char()]
+        @type dna_bitstring :: <<_::_*4>>
+
+        @spec encode_nucleotide(nucleotide_char()) :: nucleotide_bitstring()
+        def encode_nucleotide(?\s), do: 0b0000
+        def encode_nucleotide(?A), do: 0b0001
+        def encode_nucleotide(?C), do: 0b0010
+        def encode_nucleotide(?G), do: 0b0100
+        def encode_nucleotide(?T), do: 0b1000
+
+        @spec decode_nucleotide(nucleotide_bitstring()) :: nucleotide_char()
+        def decode_nucleotide(0b0000), do: ?\s
+        def decode_nucleotide(0b0001), do: ?A
+        def decode_nucleotide(0b0010), do: ?C
+        def decode_nucleotide(0b0100), do: ?G
+        def decode_nucleotide(0b1000), do: ?T
+
+        @spec encode(dna_charlist()) :: dna_bitstring()
+        def encode([]), do: <<>>
+        def encode([head | tail]), do: <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
+
+        @spec decode(bitstring) :: dna_charlist()
+        def decode(<<>>), do: []
+        def decode(<<head::4, rest::bitstring>>), do: [decode_nucleotide(head) | decode(rest)]
+      end,
+      defmodule DNA do
+        @nucleic_space 0b0000
+        @nucleic_acid_a 0b0001
+        @nucleic_acid_c 0b0010
+        @nucleic_acid_g 0b0100
+        @nucleic_acid_t 0b1000
+
+        @spec encode_nucleotide(char()) :: integer()
+        def encode_nucleotide(?\s), do: @nucleic_space
+        def encode_nucleotide(?A), do: @nucleic_acid_a
+        def encode_nucleotide(?C), do: @nucleic_acid_c
+        def encode_nucleotide(?G), do: @nucleic_acid_g
+        def encode_nucleotide(?T), do: @nucleic_acid_t
+
+        @spec decode_nucleotide(integer()) :: char()
+        def decode_nucleotide(@nucleic_space), do: ?\s
+        def decode_nucleotide(@nucleic_acid_a), do: ?A
+        def decode_nucleotide(@nucleic_acid_c), do: ?C
+        def decode_nucleotide(@nucleic_acid_g), do: ?G
+        def decode_nucleotide(@nucleic_acid_t), do: ?T
+
+        @spec encode(charlist()) :: bitstring()
+        def encode([]), do: <<>>
+        def encode([?\s | rest]), do: <<@nucleic_space::size(4), encode(rest)::bitstring>>
+        def encode([?A | rest]), do: <<@nucleic_acid_a::size(4), encode(rest)::bitstring>>
+        def encode([?C | rest]), do: <<@nucleic_acid_c::size(4), encode(rest)::bitstring>>
+        def encode([?G | rest]), do: <<@nucleic_acid_g::size(4), encode(rest)::bitstring>>
+        def encode([?T | rest]), do: <<@nucleic_acid_t::size(4), encode(rest)::bitstring>>
+
+        @spec decode(bitstring()) :: charlist()
+        def decode(<<>>), do: []
+        def decode(<<@nucleic_space::size(4), rest::bitstring>>), do: [?\s | decode(rest)]
+        def decode(<<@nucleic_acid_a::size(4), rest::bitstring>>), do: [?A | decode(rest)]
+        def decode(<<@nucleic_acid_c::size(4), rest::bitstring>>), do: [?C | decode(rest)]
+        def decode(<<@nucleic_acid_g::size(4), rest::bitstring>>), do: [?G | decode(rest)]
+        def decode(<<@nucleic_acid_t::size(4), rest::bitstring>>), do: [?T | decode(rest)]
+      end
+    ]
+  end
 end
