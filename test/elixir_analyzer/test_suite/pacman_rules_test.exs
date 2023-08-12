e3e604e0d3fcff9ffbbf5e15b78145f7d0c7befe
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-08-12 21:15:46 +0200

    Update elixir submodule

--- test/elixir_analyzer/test_suite/pacman_rules_test.exs
+++ test/elixir_analyzer/test_suite/pacman_rules_test.exs
@@ -5,20 +5,20 @@ defmodule ElixirAnalyzer.ExerciseTest.PacmanRulesTest do
   test_exercise_analysis "example solution",
     comments: [ElixirAnalyzer.Constants.solution_same_as_exemplar()] do
     defmodule Rules do
-      def eat_ghost?(power_pellet_active, touching_ghost) do
-        power_pellet_active and touching_ghost
+      def eat_ghost?(power_pellet_active?, touching_ghost?) do
+        power_pellet_active? and touching_ghost?
       end
 
-      def score?(touching_power_pellet, touching_dot) do
-        touching_power_pellet or touching_dot
+      def score?(touching_power_pellet?, touching_dot?) do
+        touching_power_pellet? or touching_dot?
       end
 
-      def lose?(power_pellet_active, touching_ghost) do
-        not power_pellet_active and touching_ghost
+      def lose?(power_pellet_active?, touching_ghost?) do
+        not power_pellet_active? and touching_ghost?
       end
 
-      def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
-        has_eaten_all_dots and not lose?(power_pellet_active, touching_ghost)
+      def win?(has_eaten_all_dots?, power_pellet_active?, touching_ghost?) do
+        has_eaten_all_dots? and not lose?(power_pellet_active?, touching_ghost?)
       end
     end
   end
@@ -28,22 +28,22 @@ defmodule ElixirAnalyzer.ExerciseTest.PacmanRulesTest do
       comments_include: [Constants.pacman_rules_use_strictly_boolean_operators()] do
       [
         defmodule Rules do
-          def eat_ghost?(power_pellet_active, touching_ghost) do
-            power_pellet_active && touching_ghost
+          def eat_ghost?(power_pellet_active?, touching_ghost?) do
+            power_pellet_active? && touching_ghost?
           end
         end,
         defmodule Rules do
-          def eat_ghost?(power_pellet_active, touching_ghost) do
-            power_pellet_active and touching_ghost
+          def eat_ghost?(power_pellet_active?, touching_ghost?) do
+            power_pellet_active? and touching_ghost?
           end
 
-          def lose?(power_pellet_active, touching_ghost) do
-            not power_pellet_active && touching_ghost
+          def lose?(power_pellet_active?, touching_ghost?) do
+            not power_pellet_active? && touching_ghost?
           end
         end,
         defmodule Rules do
-          def eat_ghost?(power_pellet_active, touching_ghost) do
-            Kernel.&&(power_pellet_active, touching_ghost)
+          def eat_ghost?(power_pellet_active?, touching_ghost?) do
+            Kernel.&&(power_pellet_active?, touching_ghost?)
           end
         end
       ]
@@ -53,13 +53,13 @@ defmodule ElixirAnalyzer.ExerciseTest.PacmanRulesTest do
       comments_include: [Constants.pacman_rules_use_strictly_boolean_operators()] do
       [
         defmodule Rules do
-          def score?(touching_power_pellet, touching_dot) do
-            touching_power_pellet || touching_dot
+          def score?(touching_power_pellet?, touching_dot?) do
+            touching_power_pellet? || touching_dot?
           end
         end,
         defmodule Rules do
-          def score?(touching_power_pellet, touching_dot) do
-            Kernel.||(touching_power_pellet, touching_dot)
+          def score?(touching_power_pellet?, touching_dot?) do
+            Kernel.||(touching_power_pellet?, touching_dot?)
           end
         end
       ]
@@ -69,32 +69,32 @@ defmodule ElixirAnalyzer.ExerciseTest.PacmanRulesTest do
       comments_include: [Constants.pacman_rules_use_strictly_boolean_operators()] do
       [
         defmodule Rules do
-          def lose?(power_pellet_active, touching_ghost) do
-            !power_pellet_active and touching_ghost
+          def lose?(power_pellet_active?, touching_ghost?) do
+            !power_pellet_active? and touching_ghost?
           end
         end,
         defmodule Rules do
-          def lose?(power_pellet_active, touching_ghost) do
-            touching_ghost and !power_pellet_active
+          def lose?(power_pellet_active?, touching_ghost?) do
+            touching_ghost? and !power_pellet_active?
           end
         end,
         defmodule Rules do
-          def lose?(power_pellet_active, touching_ghost) do
-            not power_pellet_active and touching_ghost
+          def lose?(power_pellet_active?, touching_ghost?) do
+            not power_pellet_active? and touching_ghost?
           end
 
-          def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
-            has_eaten_all_dots and !lose?(power_pellet_active, touching_ghost)
+          def win?(has_eaten_all_dots?, power_pellet_active?, touching_ghost?) do
+            has_eaten_all_dots? and !lose?(power_pellet_active?, touching_ghost?)
           end
         end,
         defmodule Rules do
-          def eat_ghost?(power_pellet_active, touching_ghost) do
-            !!power_pellet_active and !!touching_ghost
+          def eat_ghost?(power_pellet_active?, touching_ghost?) do
+            !!power_pellet_active? and !!touching_ghost?
           end
         end,
         defmodule Rules do
-          def lose?(power_pellet_active, touching_ghost) do
-            Kernel.!(power_pellet_active) and touching_ghost
+          def lose?(power_pellet_active?, touching_ghost?) do
+            Kernel.!(power_pellet_active?) and touching_ghost?
           end
         end
       ]
