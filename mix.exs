a9a7ff0519c32e61dab3ac324a24c3e710cd4ab9
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-12-26 15:19:35 +0100

    Elixir 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirAnalyzer.MixProject do
     [
       app: :elixir_analyzer,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       # Turn off protocol consolidation to avoid warning in analyzed code
